8261364071c4c42c060a5c3e3b915b3aa3da63ff
Gustav <gustav.bjordal@it.uu.se>
2020-05-19 12:33:26 +0200

    cleaning files

--- build/ext/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/frekn832/cbls/ext/googletest/googletest
-# Build directory: /home/frekn832/cbls/build/ext/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
