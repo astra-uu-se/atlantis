acf397316fee7130fedbb0df8f53ce2356e7616a
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2020-05-25 13:19:27 +0200

    Changed from typedef to using

--- include/base/intSize.hpp
+++ include/base/intSize.hpp
@@ -3,9 +3,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
-typedef u_int64_t UInt64;
-typedef u_int32_t UInt32;
-
-typedef UInt64 UInt;
-typedef int64_t Int;
+using UInt64 = u_int64_t;
+using UInt32 = u_int32_t;
+using UInt = UInt64;
+using Int = int64_t;
 #endif /* SRC_BASE_INTSIZE_H_ */
