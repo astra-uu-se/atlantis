1286fed1a3d9cdd926e129ad1d7c71a3b1d409f8
Gustav <gustav.bjordal@it.uu.se>
2020-06-01 17:03:12 +0200

    still figuring out notification API

--- src/invariants/linear.cpp
+++ src/invariants/linear.cpp
@@ -4,6 +4,8 @@ Linear::Linear(Engine& e, Id id, std::vector<Int>&& A,
                std::vector<std::weak_ptr<IntVar>>&& X, Int c)
     : Invariant(id), m_A(std::move(A)), m_X(std::move(X)), m_c(c) {
   for (auto x : m_X) {
-    e.registerDependency(m_id, x.lock()->m_id);
+    // if (auto xPtr = x.lock()) {
+    //   e.registerDependency(m_id, xPtr->m_id, []() { return i; });
+    // }
   }
 }
