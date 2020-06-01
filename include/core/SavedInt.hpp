21ac50acdbe2413fce4ffd38e702dc76cac6916a
Gustav <gustav.bjordal@it.uu.se>
2020-06-01 12:47:23 +0200

    fixed bug in savedInt

--- include/core/SavedInt.hpp
+++ include/core/SavedInt.hpp
@@ -3,31 +3,29 @@
 #include "types.hpp"
 
 class SavedInt {
-  private:
+ private:
   Int m_savedValue;
   Int m_tmpValue;
   Timestamp m_tmpTime;
 
-  public:
-
+ public:
   SavedInt(Timestamp initTime, Int initValue)
       : m_savedValue(initValue), m_tmpValue(initValue), m_tmpTime(initTime) {}
 
-  __forceinline Int getValue(Timestamp currentTime) noexcept {
+  inline Int getValue(Timestamp currentTime) noexcept {
     return currentTime == m_tmpTime ? m_tmpValue : (m_tmpValue = m_savedValue);
   }
 
-  __forceinline Int peekValue(Timestamp currentTime) noexcept {
+  inline Int peekValue(Timestamp currentTime) noexcept {
     return currentTime == m_tmpTime ? m_tmpValue : m_savedValue;
   }
 
-  __forceinline void setValue(Timestamp currentTime, Int value) noexcept {
+  inline void setValue(Timestamp currentTime, Int value) noexcept {
     m_tmpTime = currentTime;
-    m_tmpValue = m_savedValue;
+    m_tmpValue = value;
   }
 
-  __forceinline void incValue(Timestamp currentTime, Int inc,
-                              bool commit) noexcept {
+  inline void incValue(Timestamp currentTime, Int inc) noexcept {
     m_tmpTime = currentTime;
     if (currentTime == m_tmpTime) {
       m_tmpValue += inc;
@@ -35,11 +33,11 @@ class SavedInt {
       m_tmpValue = m_savedValue + inc;
     }
   }
-  __forceinline void setValueCommit(Int value) noexcept { m_savedValue = value; }
+  inline void commitValue(Int value) noexcept { m_savedValue = value; }
 
-  __forceinline void commit() noexcept { m_savedValue = m_tmpValue; }
+  inline void commit() noexcept { m_savedValue = m_tmpValue; }
 
-  __forceinline void commitIf(Timestamp currentTime) noexcept {
+  inline void commitIf(Timestamp currentTime) noexcept {
     if (m_tmpTime == currentTime) {
       m_savedValue = m_tmpValue;
     }
