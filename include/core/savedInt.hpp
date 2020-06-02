79ca895183e64a60c0eea518e2c6049a7b4f6c45
Gustav <gustav.bjordal@it.uu.se>
2020-06-02 14:22:27 +0200

    added some attributes

--- include/core/savedInt.hpp
+++ include/core/savedInt.hpp
@@ -17,21 +17,21 @@ class SavedInt {
   //   m_savedValue);
   // }
 
-  inline Int getValue(const Timestamp& currentTime) noexcept {
+  [[gnu::always_inline]] inline Int getValue(const Timestamp& currentTime) noexcept {
     return currentTime == m_tmpTime ? m_tmpValue : m_savedValue;
   }
 
-  inline Int getCommittedValue() noexcept {
+  [[gnu::always_inline]] inline Int getCommittedValue() noexcept {
     return m_savedValue;
   }
 
-  inline void setValue(const Timestamp& currentTime,
+  [[gnu::always_inline]] inline void setValue(const Timestamp& currentTime,
                        const Int& value) noexcept {
     m_tmpTime = currentTime;
     m_tmpValue = value;
   }
 
-  inline void incValue(const Timestamp& currentTime, const Int& inc) noexcept {
+  [[gnu::always_inline]] inline void incValue(const Timestamp& currentTime, const Int& inc) noexcept {
     if (currentTime == m_tmpTime) {
       m_tmpValue += inc;
     } else {
@@ -39,11 +39,11 @@ class SavedInt {
     }
     m_tmpTime = currentTime;
   }
-  inline void commitValue(Int value) noexcept { m_savedValue = value; }
+  [[gnu::always_inline]] inline void commitValue(Int value) noexcept { m_savedValue = value; }
 
-  inline void commit() noexcept { m_savedValue = m_tmpValue; }
+  [[gnu::always_inline]] inline void commit() noexcept { m_savedValue = m_tmpValue; }
 
-  inline void commitIf(const Timestamp& currentTime) noexcept {
+  [[gnu::always_inline]] inline void commitIf(const Timestamp& currentTime) noexcept {
     if (m_tmpTime == currentTime) {
       m_savedValue = m_tmpValue;
     }
--- src/invariants/linear.cpp
+++ src/invariants/linear.cpp
@@ -1,11 +1,10 @@
 #include "invariants/linear.hpp"
 #include <vector>
 
-Linear::Linear(Engine& e, Id id, std::vector<Int>&& A,
+Linear::Linear([[maybe_unused]] Engine& e, Id id, std::vector<Int>&& A,
                std::vector<std::shared_ptr<IntVar>>&& X)
     : Invariant(id), m_A(std::move(A)), m_X(std::move(X)) {}
 
-void Linear::notifyIntChanged(Engine& e, Id id, Int oldValue, Int newValue,
-                              Int data) {
-
-                              }
+void Linear::notifyIntChanged([[maybe_unused]] Engine& e, [[maybe_unused]] Id id,
+                              [[maybe_unused]] Int oldValue, [[maybe_unused]] Int newValue,
+                              [[maybe_unused]] Int data) {}
