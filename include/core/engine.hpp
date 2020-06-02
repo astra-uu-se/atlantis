928559e4b6537c18627b7d6c8157e55c76aa2030
Gustav <gustav.bjordal@it.uu.se>
2020-06-02 16:12:52 +0200

    More invariant API

--- include/core/engine.hpp
+++ include/core/engine.hpp
@@ -1,16 +1,18 @@
 #pragma once
-#include "core/types.hpp"
 #include "core/invariant.hpp"
+#include "core/types.hpp"
 
 class Engine {
  private:
-
  public:
   static const Id NULL_ID = 0;
   Engine(/* args */);
   ~Engine();
 
+  //---------------------Notificaion
+  void notifyMaybeChanged(Int id);
 
+  //---------------------Registration
   /**
    * Register an invariant in the engine and return its new id.
    * TODO: this should also set the id of the invariant to the new id.
@@ -23,7 +25,8 @@ class Engine {
   void registerDependency(Id target, Id dependency, Id localId, Int data);
 
   /**
-   * Register that source defines variable definedVar. Throws exception if already defined.
+   * Register that source defines variable definedVar. Throws exception if
+   * already defined.
    */
   void registerDefinedVariable(Id source, Id definedVar);
 };
