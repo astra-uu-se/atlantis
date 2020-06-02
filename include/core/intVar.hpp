ac3411f51bc78fb40bfc1dfce4ea8f6acff8d580
Gustav <gustav.bjordal@it.uu.se>
2020-06-02 15:45:03 +0200

    improved IntVar and savedInt

--- include/core/intVar.hpp
+++ include/core/intVar.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "core/savedInt.hpp"
+#include "core/types.hpp"
 #include "core/var.hpp"
 
 class IntVar : public Var {
@@ -12,6 +13,25 @@ class IntVar : public Var {
   IntVar(Id t_id);
   ~IntVar();
 
-  [[gnu::always_inline]] inline Int getNewValue(const Timestamp& t) { return m_value.getValue(t); }
-  [[gnu::always_inline]] inline Int getOldValue() { return m_value.getCommittedValue(); }
+  [[gnu::always_inline]] inline Int getNewValue(const Timestamp& t) {
+    return m_value.getValue(t);
+  }
+  [[gnu::always_inline]] inline Int getOldValue() {
+    return m_value.getCommittedValue();
+  }
+  [[gnu::always_inline]] inline void setNewValue(const Timestamp& timestamp,
+                                                Int value) {
+    m_value.setValue(timestamp, value);
+  }
+  [[gnu::always_inline]] inline void incValue(const Timestamp& timestamp,
+                                             Int inc) {
+    m_value.incValue(timestamp, inc);
+  }
+  [[gnu::always_inline]] inline void commit() { m_value.commit(); }
+  [[gnu::always_inline]] inline void commitValue(Int value) {
+    m_value.commitValue(value);
+  }
+  [[gnu::always_inline]] inline void commitIf(const Timestamp& timestamp) {
+    m_value.commitValue(timestamp);
+  }
 };
