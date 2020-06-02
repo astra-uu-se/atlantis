b15b95f8e73e5a3c30b598448591afde6f10ae1b
Gustav <gustav.bjordal@it.uu.se>
2020-06-02 17:15:52 +0200

    Better typesafety for IDs

--- include/invariants/linear.hpp
+++ include/invariants/linear.hpp
@@ -27,7 +27,7 @@ class Linear : public Invariant {
 
   ~Linear();
   void init(Engine& e) override;
-  void notifyIntChanged(const Timestamp& t, Engine& e, Id id, Int oldValue,
+  void notifyIntChanged(const Timestamp& t, Engine& e, VarId id, Int oldValue,
                         Int newValue, Int data) override;
   void commit(const Timestamp& t) override;
 };
--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,5 +1,7 @@
 #include "core/engine.hpp"
 
+const Id Engine::NULL_ID = Id(0);
 Engine::Engine(/* args */) {}
 
 Engine::~Engine() {}
+
