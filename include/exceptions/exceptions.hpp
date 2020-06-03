d91481d7aa56aaa6fa736a140b5448f030504f6d
Gustav <gustav.bjordal@it.uu.se>
2020-06-03 16:59:00 +0200

    Cleaning exception class, why was it written that way?

--- include/exceptions/exceptions.hpp
+++ include/exceptions/exceptions.hpp
@@ -1,37 +1,16 @@
 #include <stdexcept>
 
-class VariableAlreadyDefinedException : virtual public std::runtime_error {
-protected:
-  int m_errorNumber;
-  int m_errorOffset;
+class VariableAlreadyDefinedException : public std::runtime_error {
 
 public:
-  /** Constructor (C++ STL string, int, int).
-   *  @param msg The error message
-   *  @param t_errorNumber Error number
-   *  @param t_errorOffset Error offset
+  /** 
+   * @param msg The error message
    */
   explicit 
-  VariableAlreadyDefinedException(const std::string& msg, int t_errorNumber, int t_errorOffset) :
-    std::runtime_error(msg),
-    m_errorNumber(t_errorNumber),
-    m_errorOffset(t_errorOffset) {}
+  VariableAlreadyDefinedException(const std::string& msg) :
+    std::runtime_error(msg) {}
 
   /** Destructor.
-   *  Virtual to allow for subclassing.
    */
-  virtual ~VariableAlreadyDefinedException() {}
-  
-  /** Returns the error number.
-   *  @return #m_errorNumber
-   */
-  virtual int getErrorNumber() const {
-    return m_errorNumber;
-  }
-  /**Returns the error offset.
-   * @return #m_errorOffset
-   */
-  virtual int getErrorOffset() const {
-    return m_errorOffset;
-  }
+  ~VariableAlreadyDefinedException() {}
 };
