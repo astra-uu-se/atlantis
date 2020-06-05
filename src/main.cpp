709c1ca977e5ad062991db97ed2de4f8ed1866fb
Gustav <gustav.bjordal@it.uu.se>
2020-06-05 14:25:36 +0200

    running example

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,65 @@ int main() {
   std::cout << "hello world!" << std::endl;
   Engine engine;
   std::shared_ptr<IntVar> a = engine.makeIntVar();
-
   std::shared_ptr<IntVar> b = engine.makeIntVar();
   std::shared_ptr<IntVar> c = engine.makeIntVar();
   std::shared_ptr<IntVar> d = engine.makeIntVar();
   std::shared_ptr<IntVar> e = engine.makeIntVar();
   std::shared_ptr<IntVar> f = engine.makeIntVar();
-  a->commitValue(5);
+  std::shared_ptr<IntVar> g = engine.makeIntVar();
+  a->commitValue(1);
+  b->commitValue(2);
+  c->commitValue(3);
+  d->commitValue(4);
+  e->commitValue(5);
+  f->commitValue(6);
+  g->commitValue(7);
 
   std::shared_ptr<Linear> abc =
       std::make_shared<Linear>(std::vector<Int>({1, 1}),
                                std::vector<std::shared_ptr<IntVar>>({a, b}), c);
   engine.registerInvariant(abc);
   abc->init(engine);
-  std::cout << "end";
+
+  std::shared_ptr<Linear> def =
+      std::make_shared<Linear>(std::vector<Int>({2, 2}),
+                               std::vector<std::shared_ptr<IntVar>>({d, e}), f);
+  engine.registerInvariant(def);
+  def->init(engine);
+
+  std::shared_ptr<Linear> acfg = std::make_shared<Linear>(
+      std::vector<Int>({3, 2, 1}),
+      std::vector<std::shared_ptr<IntVar>>({a, c, f}), g);
+  engine.registerInvariant(acfg);
+  acfg->init(engine);
+  std::cout << "----- end setup -----\n";
+
+  std::cout << "----- timestamp 1 -----\n";
+  Timestamp timestamp = 0;
+  {
+    timestamp = 1;
+    a->setNewValue(timestamp, 2);
+    abc->notifyIntChanged(timestamp, engine, 0, 1, 2, 1);
+    std::cout << "new value of c: " << c->getValue(timestamp) << "\n";
+    acfg->notifyIntChanged(timestamp, engine, 0, a->getCommittedValue(),
+                           a->getValue(timestamp), 3);
+    std::cout << "new value of g: " << g->getValue(timestamp) << "\n";
+    acfg->notifyIntChanged(timestamp, engine, 1, c->getCommittedValue(),
+                           c->getValue(timestamp), 2);
+    std::cout << "new value of g: " << g->getValue(timestamp) << "\n";
+  }
+  std::cout << "----- timestamp 2 -----\n";
+  {
+    timestamp = 2;
+    a->setNewValue(timestamp, 0);
+    abc->notifyIntChanged(timestamp, engine, 0, a->getCommittedValue(),
+                          a->getValue(timestamp), 1);
+    std::cout << "new value of c: " << c->getValue(timestamp) << "\n";
+    acfg->notifyIntChanged(timestamp, engine, 0, a->getCommittedValue(),
+                           a->getValue(timestamp), 3);
+    std::cout << "new value of g: " << g->getValue(timestamp) << "\n";
+    acfg->notifyIntChanged(timestamp, engine, 1, c->getCommittedValue(),
+                           c->getValue(timestamp), 2);
+    std::cout << "new value of g: " << g->getValue(timestamp) << "\n";
+  }
 }
