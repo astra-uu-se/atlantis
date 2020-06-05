0d66c6cfbcb927a82e3c0acaea21a995dee0f390
Gustav <gustav.bjordal@it.uu.se>
2020-06-05 11:53:14 +0200

    fixed bugs in engine

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -7,13 +7,13 @@ Engine::Engine(/* args */)
     : m_intVars(),
       m_invariants(),
       m_definingInvariant(),
-      m_definedVariablesByInvariant(),
+      m_variablesDefinedByInvariant(),
       m_listeningInvariants(),
       m_modifiedVariables() {
   m_intVars.reserve(ESTIMATED_NUM_OBJECTS);
   m_invariants.reserve(ESTIMATED_NUM_OBJECTS);
   m_definingInvariant.reserve(ESTIMATED_NUM_OBJECTS);
-  m_definedVariablesByInvariant.reserve(ESTIMATED_NUM_OBJECTS);
+  m_variablesDefinedByInvariant.reserve(ESTIMATED_NUM_OBJECTS);
   m_listeningInvariants.reserve(ESTIMATED_NUM_OBJECTS);
 
   // Vectors indexed by IDs are initialised to size 1 so that the nullID is its
@@ -22,7 +22,7 @@ Engine::Engine(/* args */)
   m_invariants.push_back(nullptr);  // expands with registerInvariant
   m_definingInvariant.push_back(
       InvariantId(NULL_ID));  // expands with registerInvariant
-  m_definedVariablesByInvariant.push_back(
+  m_variablesDefinedByInvariant.push_back(
       {});                              // expands with registerInvariant
   m_listeningInvariants.push_back({});  // expands with registerIntVar
 }
@@ -32,8 +32,8 @@ Engine::~Engine() {}
 //---------------------Notificaion---------------------
 void Engine::notifyMaybeChanged(Int id) {
   // If first time variable is invalidated:
-  if (m_intVars[id]->m_isInvalid) {
-    m_intVars[id]->invalidate();
+  if (m_intVars.at(id)->m_isInvalid) {
+    m_intVars.at(id)->invalidate();
     m_modifiedVariables.push(id);
   }
 }
@@ -42,10 +42,9 @@ void Engine::notifyMaybeChanged(Int id) {
 
 InvariantId Engine::registerInvariant(std::shared_ptr<Invariant> invariantPtr) {
   m_invariants.push_back(invariantPtr);
-  m_definingInvariant.push_back(InvariantId(NULL_ID));
-  m_definedVariablesByInvariant.push_back({});
-  assert(m_invariants.size() == m_definingInvariant.size());
-  InvariantId newId = InvariantId(m_intVars.size() - 1);
+  m_variablesDefinedByInvariant.push_back({});
+  assert(m_invariants.size() == m_variablesDefinedByInvariant.size());
+  InvariantId newId = InvariantId(m_invariants.size() - 1);
   invariantPtr->setId(newId);
   return newId;
 }
@@ -53,14 +52,16 @@ InvariantId Engine::registerInvariant(std::shared_ptr<Invariant> invariantPtr) {
 VarId Engine::registerIntVar(std::shared_ptr<IntVar> intVarPtr) {
   m_intVars.push_back(intVarPtr);
   m_listeningInvariants.push_back({});
+  m_definingInvariant.push_back(InvariantId(NULL_ID));
   assert(m_intVars.size() == m_listeningInvariants.size());
+  assert(m_intVars.size() == m_definingInvariant.size());
   VarId newId = VarId(m_intVars.size() - 1);
   intVarPtr->setId(newId);
   return newId;
 }
 
 void Engine::registerIntVar(std::vector<std::shared_ptr<IntVar>> intVarPtrs) {
-  for(auto p: intVarPtrs){
+  for (auto p : intVarPtrs) {
     registerIntVar(p);
   }
 }
@@ -70,16 +71,18 @@ void Engine::registerInvariantDependency(InvariantId to, VarId from,
   assert(!to.equals(NULL_ID) && !from.equals(NULL_ID));
   m_listeningInvariants.at(from).emplace_back(
       InvariantDependencyData{to, localId, data});
+  m_variablesDefinedByInvariant.push_back({});
 }
 
 void Engine::registerDefinedVariable(InvariantId from, VarId to) {
   assert(!to.equals(NULL_ID) && !from.equals(NULL_ID));
-  if (m_definingInvariant[to].id == NULL_ID.id) {
-    m_definingInvariant[to] = from;
-    m_definedVariablesByInvariant[from].push_back(to);
+  if (m_definingInvariant.at(to).id == NULL_ID.id) {
+    m_definingInvariant.at(to) = from;
+    std::cout << m_definingInvariant.at(to) << " " << from << "\n";
+    m_variablesDefinedByInvariant.at(from).push_back(to);
   } else {
     throw new VariableAlreadyDefinedException(
         "Variable " + std::to_string(to.id) + " already defined by invariant " +
-        std::to_string(m_definingInvariant[to].id));
+        std::to_string(m_definingInvariant.at(to).id));
   }
 }
