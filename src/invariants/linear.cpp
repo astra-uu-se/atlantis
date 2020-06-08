5aced477e7bf0d87b8a6b0013dc72a060cd77f94
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2020-06-08 16:05:19 +0200

    Changed some parameter names in engine

--- src/invariants/linear.cpp
+++ src/invariants/linear.cpp
@@ -29,7 +29,7 @@ void Linear::init(const Timestamp& t, Engine& e) {
   // is initialised.
   assert(m_id != Engine::NULL_ID);
 
-  e.registerDefinedVariable(m_id, m_b->m_id);
+  e.registerDefinedVariable(m_b->m_id, m_id);
   for (size_t i = 0; i < m_X.size(); i++) {
     e.registerInvariantDependency(m_id, m_X[i]->m_id, LocalId(i), m_A[i]);
   }
