51e6b43c0af4e0b3a68f71dd24c300abf59e8680
Gustav <gustav.bjordal@it.uu.se>
2020-06-09 15:03:12 +0200

    Refactoring engine

--- include/core/engine.hpp
+++ include/core/engine.hpp
@@ -10,6 +10,7 @@
 #include "core/invariant.hpp"
 #include "core/tracer.hpp"
 #include "core/types.hpp"
+#include "propagation/propagationGraph.hpp"
 
 class Engine {
  private:
@@ -20,37 +21,7 @@ class Engine {
   std::vector<std::shared_ptr<IntVar>> m_intVars;
   std::vector<std::shared_ptr<Invariant>> m_invariants;
 
-  /**
-   * Map from VarID -> InvariantId
-   *
-   * Maps to nullptr if not defined by any invariant.
-   */
-  std::vector<InvariantId> m_definingInvariant;
-
-  /**
-   * Map from InvariantId -> list of VarID
-   *
-   * Maps an invariant to all variables it defines.
-   */
-  std::vector<std::vector<VarId>> m_variablesDefinedByInvariant;
-
-  struct InvariantDependencyData {
-    InvariantId id;
-    LocalId localId;
-    Int data;
-  };
-  // Map from VarID -> vector of InvariantID
-  std::vector<std::vector<InvariantDependencyData>> m_listeningInvariants;
-
-  std::queue<VarId> m_modifiedVariables;
-
-  /**
-   * TODO: Delete these
-   * Register an intVar in the engine and return its new id.
-   * This also sets the id of the variable to the new id.
-   */
-  VarId registerIntVar(std::shared_ptr<IntVar>);
-  void registerIntVar(std::vector<std::shared_ptr<IntVar>>);
+  PropagationGraph m_propGraph;
 
  public:
   static const Id NULL_ID;
@@ -61,25 +32,32 @@ class Engine {
   void beginMove(Timestamp& t);
   void endMove(Timestamp& t);
 
-  //--------------------- Notificaion/Modification ---------------------
+  //--------------------- Notificaion ---------------------
   /***
-  * @param t the timestamp when the changed happened
-  * @param id the id of the changed variable
-  */
+   * @param t the timestamp when the changed happened
+   * @param id the id of the changed variable
+   */
   void notifyMaybeChanged(const Timestamp& t, Id id);
 
-  void incValue(const Timestamp&, IntVar&, Int inc);
-  void setValue(const Timestamp&, IntVar&, Int val);
+  //--------------------- Variable ---------------------
+  void incValue(const Timestamp&, VarId&, Int inc);
+  void setValue(const Timestamp&, VarId&, Int val);
 
-  void commit(IntVar&); //todo: this feels dangerous, maybe commit should always have a timestamp?
-  void commitIf(const Timestamp&, IntVar&);
-  void commitValue(const Timestamp&, IntVar&, Int val);
+  Int getValue(const Timestamp&, VarId&);
+  Int getCommitedValue(VarId&);
+
+  Timestamp getTimestamp(VarId&);
+
+  void commit(VarId&);  // todo: this feels dangerous, maybe commit should
+                        // always have a timestamp?
+  void commitIf(const Timestamp&, VarId&);
+  void commitValue(const Timestamp&, VarId&, Int val);
 
   //--------------------- Registration ---------------------
   /**
    * Register an invariant in the engine and return its new id.
    * This also sets the id of the invariant to the new id.
-   * @param invariantPtr pointer to the invariant to register 
+   * @param invariantPtr pointer to the invariant to register
    * @return the id of the invariant in the engine.
    */
   InvariantId registerInvariant(std::shared_ptr<Invariant> invariantPtr);
@@ -88,7 +66,7 @@ class Engine {
    * Creates an IntVar and registers it to the engine.
    * @return the created IntVar
    */
-  std::shared_ptr<IntVar> makeIntVar();
+  VarId makeIntVar();
 
   /**
    * Register that Invariant to depends on variable from depends on dependency
@@ -97,8 +75,8 @@ class Engine {
    * @param localId the id of the depending variable in the invariant
    * @param data additioonal data
    */
-  void registerInvariantDependency(InvariantId dependee, VarId source,
-                                   LocalId localId, Int data);
+  void registerInvariantDependsOnVar(InvariantId dependee, VarId source,
+                                     LocalId localId, Int data);
 
   /**
    * Register that 'from' defines variable 'to'. Throws exception if
--- test/core/tLinear.cpp
+++ test/core/tLinear.cpp
@@ -21,21 +21,20 @@ class LinearTest : public ::testing::Test {
     b = e->makeIntVar();
     c = e->makeIntVar();
     d = e->makeIntVar();
-    e->commitValue((Timestamp)0,*a,1);
-    e->commitValue((Timestamp)0,*b,2);
-    e->commitValue((Timestamp)0,*c,3);
-    e->commitValue((Timestamp)0,*d,4);
+    e->commitValue((Timestamp)0, a, 1);
+    e->commitValue((Timestamp)0, b, 2);
+    e->commitValue((Timestamp)0, c, 3);
+    e->commitValue((Timestamp)0, d, 4);
 
     // d = 1*1+2*10+3*(-20) = 1+20-60 =-39
-    linear = std::make_shared<Linear>(
-        std::vector<Int>({1, 10, -20}),
-        std::vector<std::shared_ptr<IntVar>>({a, b, c}), d);
+    linear = std::make_shared<Linear>(std::vector<Int>({1, 10, -20}),
+                                      std::vector<VarId>({a, b, c}), d);
   }
   std::unique_ptr<Engine> e;
-  std::shared_ptr<IntVar> a;
-  std::shared_ptr<IntVar> b;
-  std::shared_ptr<IntVar> c;
-  std::shared_ptr<IntVar> d;
+  VarId a = NULL_ID;
+  VarId b = NULL_ID;
+  VarId c = NULL_ID;
+  VarId d = NULL_ID;
   std::shared_ptr<Linear> linear;
   std::mt19937 gen;
 };
@@ -46,64 +45,64 @@ class LinearTest : public ::testing::Test {
 
 TEST_F(LinearTest, Init) {
   e->registerInvariant(linear);
-  EXPECT_EQ(d->getCommittedValue(), -39);
-  EXPECT_EQ(d->getValue(d->getTimestamp()), -39);
+  EXPECT_EQ(e->getCommitedValue(d), -39);
+  EXPECT_EQ(e->getValue(e->getTimestamp(d), d), -39);
 }
 
 TEST_F(LinearTest, Recompute) {
   e->registerInvariant(linear);
 
-  EXPECT_EQ(d->getValue(0), -39);
-  EXPECT_EQ(d->getCommittedValue(), -39);
+  EXPECT_EQ(e->getValue(0, d), -39);
+  EXPECT_EQ(e->getCommitedValue(d), -39);
 
   Timestamp newTime = 1;
-  e->setValue(newTime,*a, 40);
+  e->setValue(newTime, a, 40);
   linear->recompute(newTime, *e);
-  EXPECT_EQ(d->getCommittedValue(), -39);
-  EXPECT_EQ(d->getValue(newTime), 0);
+  EXPECT_EQ(e->getCommitedValue(d), -39);
+  EXPECT_EQ(e->getValue(newTime, d), 0);
 }
 
 TEST_F(LinearTest, NotifyChange) {
   e->registerInvariant(linear);
 
-  EXPECT_EQ(d->getValue(0), -39);  // initially the value of d is -39
+  EXPECT_EQ(e->getValue(0, d), -39);  // initially the value of d is -39
 
   LocalId unused = -1;
 
   Timestamp time1 = 1;
 
-  EXPECT_EQ(a->getValue(time1), 1);
-  e->setValue(time1,*a, 40);
-  EXPECT_EQ(a->getCommittedValue(), 1);
-  EXPECT_EQ(a->getValue(time1), 40);
-  linear->notifyIntChanged(time1, *e, unused, a->getCommittedValue(),
-                           a->getValue(time1), 1);
-  EXPECT_EQ(d->getValue(time1), 0);  // incremental value of d is 0;
+  EXPECT_EQ(e->getValue(time1, a), 1);
+  e->setValue(time1, a, 40);
+  EXPECT_EQ(e->getCommitedValue(a), 1);
+  EXPECT_EQ(e->getValue(time1, a), 40);
+  linear->notifyIntChanged(time1, *e, unused, e->getCommitedValue(a),
+                           e->getValue(time1, a), 1);
+  EXPECT_EQ(e->getValue(time1, d), 0);  // incremental value of d is 0;
 
-  e->setValue(time1,*b, 0);
-  linear->notifyIntChanged(time1, *e, unused, b->getCommittedValue(),
-                           b->getValue(time1), 10);
-  auto tmpValue = d->getValue(time1);  // incremental value of d is -40;
+  e->setValue(time1, b, 0);
+  linear->notifyIntChanged(time1, *e, unused, e->getCommitedValue(b),
+                           e->getValue(time1, b), 10);
+  auto tmpValue = e->getValue(time1, d);  // incremental value of d is -40;
 
   // Incremental computation gives the same result as recomputation
   linear->recompute(time1, *e);
-  EXPECT_EQ(d->getValue(time1), tmpValue);
+  EXPECT_EQ(e->getValue(time1, d), tmpValue);
 
   Timestamp time2 = time1 + 1;
 
-  EXPECT_EQ(a->getValue(time2), 1);
-  e->setValue(time2,*a, 20);
-  EXPECT_EQ(a->getCommittedValue(), 1);
-  EXPECT_EQ(a->getValue(time2), 20);
-  linear->notifyIntChanged(time2, *e, unused, a->getCommittedValue(),
-                           a->getValue(time2), 1);
-  EXPECT_EQ(d->getValue(time2), -20);  // incremental value of d is 0;
+  EXPECT_EQ(e->getValue(time2, a), 1);
+  e->setValue(time2, a, 20);
+  EXPECT_EQ(e->getCommitedValue(a), 1);
+  EXPECT_EQ(e->getValue(time2, a), 20);
+  linear->notifyIntChanged(time2, *e, unused, e->getCommitedValue(a),
+                           e->getValue(time2, a), 1);
+  EXPECT_EQ(e->getValue(time2, d), -20);  // incremental value of d is 0;
 }
 
 TEST_F(LinearTest, IncrementalVsRecompute) {
   e->registerInvariant(linear);
 
-  EXPECT_EQ(d->getValue(0), -39);  // initially the value of d is -39
+  EXPECT_EQ(e->getValue(0, d), -39);  // initially the value of d is -39
   LocalId unused = -1;
   // todo: not clear if we actually want to deal with overflows...
   std::uniform_int_distribution<> distribution(-100000, 100000);
@@ -112,57 +111,57 @@ TEST_F(LinearTest, IncrementalVsRecompute) {
   for (size_t i = 0; i < 1000; ++i) {
     ++currentTime;
     // Check that we do not accidentally commit
-    EXPECT_EQ(a->getCommittedValue(), 1);
-    EXPECT_EQ(b->getCommittedValue(), 2);
-    EXPECT_EQ(c->getCommittedValue(), 3);
-    EXPECT_EQ(d->getCommittedValue(), -39);  // d is commited by register.
+    EXPECT_EQ(e->getCommitedValue(a), 1);
+    EXPECT_EQ(e->getCommitedValue(b), 2);
+    EXPECT_EQ(e->getCommitedValue(c), 3);
+    EXPECT_EQ(e->getCommitedValue(d), -39);  // d is commited by register.
 
     // Set all variables
-    e->setValue(currentTime,*a, distribution(gen));
-    e->setValue(currentTime,*b, distribution(gen));
-    e->setValue(currentTime,*c, distribution(gen));
+    e->setValue(currentTime, a, distribution(gen));
+    e->setValue(currentTime, b, distribution(gen));
+    e->setValue(currentTime, c, distribution(gen));
 
     // notify changes
-    if (a->getCommittedValue() != a->getValue(currentTime)) {
-      linear->notifyIntChanged(currentTime, *e, unused, a->getCommittedValue(),
-                               a->getValue(currentTime), 1);
+    if (e->getCommitedValue(a) != e->getValue(currentTime, a)) {
+      linear->notifyIntChanged(currentTime, *e, unused, e->getCommitedValue(a),
+                               e->getValue(currentTime, a), 1);
     }
-    if (b->getCommittedValue() != b->getValue(currentTime)) {
-      linear->notifyIntChanged(currentTime, *e, unused, b->getCommittedValue(),
-                               b->getValue(currentTime), 10);
+    if (e->getCommitedValue(b) != e->getValue(currentTime, b)) {
+      linear->notifyIntChanged(currentTime, *e, unused, e->getCommitedValue(b),
+                               e->getValue(currentTime, b), 10);
     }
-    if (c->getCommittedValue() != c->getValue(currentTime)) {
-      linear->notifyIntChanged(currentTime, *e, unused, c->getCommittedValue(),
-                               c->getValue(currentTime), -20);
+    if (e->getCommitedValue(c) != e->getValue(currentTime, c)) {
+      linear->notifyIntChanged(currentTime, *e, unused, e->getCommitedValue(c),
+                               e->getValue(currentTime, c), -20);
     }
 
     // incremental value
-    auto tmp = d->getValue(currentTime);
+    auto tmp = e->getValue(currentTime, d);
     linear->recompute(currentTime, *e);
 
-    EXPECT_EQ(tmp, d->getValue(currentTime));
+    EXPECT_EQ(tmp, e->getValue(currentTime, d));
   }
 }
 
 TEST_F(LinearTest, Commit) {
   e->registerInvariant(linear);
-  EXPECT_EQ(d->getCommittedValue(), -39);
+  EXPECT_EQ(e->getCommitedValue(d), -39);
 
   LocalId unused = -1;
 
   Timestamp currentTime = 1;
 
-  e->setValue(currentTime,*a, 40);
-  e->setValue(currentTime,*b, 2);  // This change is not notified and should not
-                                // have an impact on the commit
+  e->setValue(currentTime, a, 40);
+  e->setValue(currentTime, b, 2);  // This change is not notified and should not
+                                   // have an impact on the commit
 
-  linear->notifyIntChanged(currentTime, *e, unused, a->getCommittedValue(),
-                           a->getValue(currentTime), 1);
+  linear->notifyIntChanged(currentTime, *e, unused, e->getCommitedValue(a),
+                           e->getValue(currentTime, a), 1);
 
   // Commit at wrong timestamp should have no impact
   linear->commit(currentTime + 1, *e);
-  EXPECT_EQ(d->getCommittedValue(), -39);
+  EXPECT_EQ(e->getCommitedValue(d), -39);
   linear->commit(currentTime, *e);
-  EXPECT_EQ(d->getCommittedValue(), 0);
+  EXPECT_EQ(e->getCommitedValue(d), 0);
 }
 }  // namespace
