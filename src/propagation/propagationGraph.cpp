ffeb4932423e8809b82609c2a3f45c28cbcc6ce0
Gustav <gustav.bjordal@it.uu.se>
2020-06-11 16:22:37 +0200

    Topological sort that merges cycles. Slow but works.

--- src/propagation/propagationGraph.cpp
+++ src/propagation/propagationGraph.cpp
@@ -6,7 +6,7 @@
 extern Id NULL_ID;
 
 PropagationGraph::PropagationGraph(size_t expectedSize)
-    : m_numInvariants(0), m_numVariables(0), m_topology(*this) {
+    : m_numInvariants(0), m_numVariables(0), m_topology(*this){
   m_definingInvariant.reserve(expectedSize);
   m_variablesDefinedByInvariant.reserve(expectedSize);
   m_listeningInvariants.reserve(expectedSize);
@@ -92,7 +92,7 @@ void PropagationGraph::registerDefinedVariable(VarId dependent,
  * Also This implementation is very inefficient, I think it is O(VE+E).  see
  * https://en.wikipedia.org/wiki/Topological_sorting for better versions.
  */
-void PropagationGraph::Topology::computeTopologyNoCycles() {
+void PropagationGraph::Topology::computeNoCycles() {
   m_variablePosition.resize(graph.m_numVariables + 1, 0);
   m_invariantPosition.resize(graph.m_numInvariants + 1, 0);
 
@@ -103,7 +103,7 @@ void PropagationGraph::Topology::computeTopologyNoCycles() {
   // Find the top level variables that are not defined by anything.
   // todo: it would be cleaning if we maintained a list of all added varIDs but
   // it is more efficient just to recreated based on the total number.
-  for (size_t i = 0; i < graph.m_numVariables+1; i++) {
+  for (size_t i = 0; i < graph.m_numVariables + 1; i++) {
     if (i == NULL_ID) {  // Skip nullVar if any.
       continue;
     }
@@ -131,22 +131,71 @@ void PropagationGraph::Topology::computeTopologyNoCycles() {
   }
 }
 
+/**
+ * Throws an exception if the graph has cycles.
+ * Uses different key-domains for variables and invariants.
+ */
+void PropagationGraph::Topology::computeNoCyclesException() {
+  std::vector<bool> tmpVisited;
+  std::vector<bool> visited;
+  tmpVisited.resize(graph.m_numVariables + 1, false);
+  visited.resize(graph.m_numVariables + 1, false);
+
+  std::queue<VarId> reverseOrder;
+
+  std::function<void(VarId)> visit = [&](VarId id) {
+    // tmp Mark current node
+    tmpVisited.at(id) = true;
+    // for each dependent invariant
+    for (auto dependencyData : graph.m_listeningInvariants.at(id)) {
+      InvariantId invariant = dependencyData.id;
+
+      // for each variable defined by that invariant
+      for (auto dependentVariable :
+           graph.m_variablesDefinedByInvariant.at(invariant)) {
+        if (visited.at(dependentVariable)) {
+          continue;
+        } else if (tmpVisited.at(dependentVariable)) {
+          throw new PropagationGraphHasCycles(
+              "var " + std::to_string(dependentVariable) +
+              " is part of cycle.");
+        }
+        visit(dependentVariable);
+      }
+    }
+    visited.at(id) = true;
+    reverseOrder.push(id);
+  };
+
+  // Call visit on all top variables
+  for (size_t i = 0; i < graph.m_numVariables + 1; i++) {
+    if (i == NULL_ID) {  // Skip nullVar if any.
+      continue;
+    }
+    if (graph.m_definingInvariant.at(i).id == NULL_ID) {
+      visit(VarId(i));
+    }
+  }
+
+  m_variablePosition.resize(graph.m_numVariables + 1, 0);
+  while (!reverseOrder.empty()) {
+    m_variablePosition.at(reverseOrder.front()) = reverseOrder.size();
+    reverseOrder.pop();
+  }
+  computeInvariantFromVariables();
+}
 
 /**
- * Computes a topological sort from a dependency graph with cycles by 
+ * Computes a topological sort from a dependency graph with cycles by
  * non-deterministically ignoring one edge in each cycle.
  * This means that there will be an order within cycles.
- * 
+ *
  * Gives different key-domains to Variables and invariants.
  * That is, the key of invariants cannot be compared with variables.
  */
-void PropagationGraph::Topology::computeTopologyWithCycles() {
-
+void PropagationGraph::Topology::computeWithCycles() {
   std::vector<bool> visited;
   visited.resize(graph.m_numVariables + 1, false);
-
-  std::vector<size_t> varPosition;
-  varPosition.resize(graph.m_numVariables + 1, 0);
 
   std::queue<VarId> reverseOrder;
 
@@ -172,7 +221,7 @@ void PropagationGraph::Topology::computeTopologyWithCycles() {
   };
 
   // Call visit on all top variables
-  for (size_t i = 0; i < graph.m_numVariables+1; i++) {
+  for (size_t i = 0; i < graph.m_numVariables + 1; i++) {
     if (i == NULL_ID) {  // Skip nullVar if any.
       continue;
     }
@@ -186,9 +235,96 @@ void PropagationGraph::Topology::computeTopologyWithCycles() {
     m_variablePosition.at(reverseOrder.front()) = reverseOrder.size();
     reverseOrder.pop();
   }
+  computeInvariantFromVariables();
+}
 
+void PropagationGraph::Topology::computeBundleCycles() {
+  m_variablePosition.resize(graph.m_numVariables + 1, 0);
+
+  std::vector<bool> isPartOfCycle;
+  isPartOfCycle.resize(graph.m_numVariables + 1, false);
+
+  std::vector<bool> tmpMark;
+  tmpMark.resize(graph.m_numVariables + 1, false);
+
+  std::function<bool(VarId, VarId)> detectCycle = [&](VarId target,
+                                                        VarId current) {
+    if (tmpMark.at(current) || isPartOfCycle.at(target)) {
+      return false;
+    }
+    tmpMark.at(current) = true;
+    bool inCycle = false;
+    for (auto dependencyData : graph.m_listeningInvariants.at(current)) {
+      InvariantId invariant = dependencyData.id;
+      // for each variable defined by that invariant
+      for (auto dependentVariable :
+           graph.m_variablesDefinedByInvariant.at(invariant)) {
+        if (dependentVariable.id == target.id) {
+          isPartOfCycle.at(target) = true;
+          inCycle = true;
+        } else {
+          inCycle |= detectCycle(target, dependentVariable);
+        }
+      }
+    }
+    isPartOfCycle.at(current) = isPartOfCycle.at(current) || inCycle;
+    // Remove mark from node so that it can be visited in different recursion.
+    tmpMark.at(current) = false;
+    return inCycle;
+  };
+
+  std::function<void(VarId, size_t)> visit = [&](VarId id, size_t pos) {
+    // If position is not 0, then return it
+    if (m_variablePosition.at(id) > pos) {
+      return;
+    }
+    m_variablePosition.at(id) = pos;
+    tmpMark.at(id) = true;
+
+    for (auto dependencyData : graph.m_listeningInvariants.at(id)) {
+      InvariantId invariant = dependencyData.id;
+      // for each variable defined by that invariant
+      for (auto dependentVariable :
+           graph.m_variablesDefinedByInvariant.at(invariant)) {
+        if (!tmpMark.at(dependentVariable)) {
+          size_t nextPos = pos + ((isPartOfCycle.at(id) &&
+                                           isPartOfCycle.at(dependentVariable)
+                                       ? 0
+                                       : 1));
+          visit(dependentVariable, nextPos);
+        }
+      }
+    }
+    tmpMark.at(id) = false;
+    return;
+  };
+
+  for (size_t i = 0; i < graph.m_numVariables + 1; i++) {
+    if (i == NULL_ID) {  // Skip nullVar if any.
+      continue;
+    }
+    detectCycle(VarId(i), VarId(i));
+  }
+
+  // Call visit on all top variables
+  for (size_t i = 0; i < graph.m_numVariables + 1; i++) {
+    if (i == NULL_ID) {  // Skip nullVar if any.
+      continue;
+    }
+    if (graph.m_definingInvariant.at(i).id == NULL_ID) {
+      visit(VarId(i), 1);
+    }
+  }
+  computeInvariantFromVariables();
+}
+
+/**
+ * Computes a topology from the current topology of variables.
+ * Notes that this gives different key-domains to Variables and invariants.
+ */
+void PropagationGraph::Topology::computeInvariantFromVariables() {
   m_invariantPosition.resize(graph.m_numInvariants + 1, 0);
-  for (size_t i = 0; i < graph.m_numInvariants+1; i++) {
+  for (size_t i = 0; i < graph.m_numInvariants + 1; i++) {
     if (i == NULL_ID) {  // Skip nullVar if any.
       continue;
     }
@@ -202,5 +338,3 @@ void PropagationGraph::Topology::computeTopologyWithCycles() {
     m_invariantPosition.at(invariant) = position;
   }
 }
-
-void PropagationGraph::Topology::computeTopologyBundleCycles() {}
