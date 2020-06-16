b9ae6fe0166643d9c58bec86ca98b6a664940cbd
Gustav <gustav.bjordal@it.uu.se>
2020-06-16 12:10:21 +0200

    changing main

--- test/core/tEngine.cpp
+++ test/core/tEngine.cpp
@@ -26,8 +26,6 @@ class EngineTest : public ::testing::Test {
 };
 
 TEST_F(EngineTest, CreateVariablesAndInvariant) {
-
-
   engine->open();
 
   auto a = engine->makeIntVar(1);
@@ -35,8 +33,6 @@ TEST_F(EngineTest, CreateVariablesAndInvariant) {
   auto c = engine->makeIntVar(1);
   auto d = engine->makeIntVar(1);
   auto e = engine->makeIntVar(1);
-
-
 
   // TODO: use some other invariants...
   engine->makeInvariant<Linear>(std::vector<Int>({1, 1}),
@@ -52,9 +48,8 @@ TEST_F(EngineTest, CreateVariablesAndInvariant) {
 }
 
 TEST_F(EngineTest, SimplePropagation) {
-
   engine->open();
-  
+
   // 5a+2b+3c -> d
   // b-c -> e
   // a+2e -> f
@@ -73,7 +68,6 @@ TEST_F(EngineTest, SimplePropagation) {
   engine->makeInvariant<Linear>(std::vector<Int>({1, 2}),
                                 std::vector<VarId>({a, e}), f);
 
-
   engine->close();
 
   // All variables are initialised to 1:
@@ -85,14 +79,14 @@ TEST_F(EngineTest, SimplePropagation) {
   ASSERT_EQ(engine->getValue(e), 0);
   ASSERT_EQ(engine->getValue(f), 1);
 
-  //Change c to -4
+  // Change c to -4
   // 5+2-12 -> d = -5
   // 1+4 -> e = 5
   // 1+10 -> f = 11
   engine->beginMove();
   engine->setValue(c, -4);
   engine->endMove();
-  
+
   engine->beginQuery();
   engine->query(d);
   engine->query(e);
@@ -102,7 +96,6 @@ TEST_F(EngineTest, SimplePropagation) {
   ASSERT_EQ(engine->getValue(d), -5);
   ASSERT_EQ(engine->getValue(e), 5);
   ASSERT_EQ(engine->getValue(f), 11);
-
 }
 
 }  // namespace
