d0a738b60169858091e62bcc8c68e5e1f4ee6390
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2020-06-18 15:12:29 +0200

    Added LessEqual constraint

---
+++ include/constraints/lessEqual.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "../core/engine.hpp"
+#include "../core/intVar.hpp"
+#include "../core/constraint.hpp"
+#include "../core/types.hpp"
+#include "../core/tracer.hpp"
+
+class LessEqual : public Constraint {
+ private:
+  Int m_a;
+  Int m_b;
+  VarId m_x;
+  VarId m_y;
+
+ public:
+  LessEqual(VarId violationId, Int a, VarId x, Int b, VarId y);
+//   Linear(Engine& e, std::vector<Int>&& A,
+//          std::vector<std::shared_ptr<IntVar>>&& X, std::shared_ptr<IntVar> b);
+
+  ~LessEqual() = default;
+  void init(const Timestamp&, Engine&) override;
+  void recompute(const Timestamp&, Engine&) override;
+  void notifyIntChanged(const Timestamp& t, Engine& e, LocalId id, Int oldValue,
+                        Int newValue, Int data) override;
+  void commit(const Timestamp&, Engine&) override;
+};
