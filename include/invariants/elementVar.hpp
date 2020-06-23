5407b52da91ccf0afd7f3e4ca3c057b617a65134
Gustav <gustav.bjordal@it.uu.se>
2020-06-23 12:58:52 +0200

    first implementaiton of Element

---
+++ include/invariants/elementVar.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "../core/engine.hpp"
+#include "../core/intVar.hpp"
+#include "../core/invariant.hpp"
+#include "../core/types.hpp"
+#include "../core/tracer.hpp"
+
+/**
+ * Invariant for b <- X[i] where X is a vector of VarId.
+ *
+ */
+
+class ElementVar : public Invariant {
+ private:
+  VarId m_i;
+  std::vector<VarId> m_X;
+  VarId m_b;
+
+ public:
+  ElementVar(VarId i, std::vector<VarId>&& X,
+         VarId b);
+  ~ElementVar() = default;
+  void init(const Timestamp&, Engine&) override;
+  void recompute(const Timestamp&, Engine&) override;
+  void notifyIntChanged(const Timestamp& t, Engine& e, LocalId id, Int oldValue,
+                        Int newValue, Int data) override;
+  void commit(const Timestamp&, Engine&) override;
+};
