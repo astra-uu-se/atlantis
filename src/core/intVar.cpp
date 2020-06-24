855fc3377f6bef7ad3f125828a922b3a2a3b07b9
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2020-06-24 10:57:05 +0200

    Reverted SavedInt changes and put bounds into IntVar

--- src/core/intVar.cpp
+++ src/core/intVar.cpp
@@ -13,7 +13,13 @@ IntVar::IntVar(Id t_id, Int initValue)
 
 IntVar::IntVar(Id t_id, Int initValue, Int t_lowerBound, Int t_upperBound)
     : Var(t_id),
-      m_value(-1, initValue, t_lowerBound, t_upperBound) // todo: We need both a time-zero (when
-                                                         // initialisation happens) but also a dummy time.
-      {
+      m_value(-1, initValue), // todo: We need both a time-zero (when
+                              // initialisation happens) but also a dummy time.
+      m_lowerBound(t_lowerBound),
+      m_upperBound(t_upperBound) {
+  if (t_lowerBound > t_upperBound) {
+    throw std::out_of_range(
+      "Lower bound must be smaller than or equal to upper bound"
+    );
+  }
 }
