61ee1da12f41e0687e3ebeb24a8408faa774bfcf
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2020-10-01 08:43:34 +0200

    Started merging

--- include/propagation/bottomUpExplorer.hpp
+++ include/propagation/bottomUpExplorer.hpp
@@ -9,11 +9,6 @@
 #include "core/types.hpp"
 #include "exceptions/exceptions.hpp"
 
-<<<<<<< HEAD:include/propagation/bottomUpPropagationGraph.hpp
-class BottomUpPropagationGraph : public PropagationGraph {
- private:
-  Engine& m_engine;
-=======
 // #include "core/propagationEngine.hpp"
 
 // Forward declare PropagationEngine
@@ -23,18 +18,10 @@ class BottomUpExplorer {
   PropagationEngine& m_engine;
 
   std::vector<VarId> variableStack_;
->>>>>>> develop:include/propagation/bottomUpExplorer.hpp
   size_t varStackIdx_ = 0;
   size_t invariantStackIdx_ = 0;
-<<<<<<< HEAD:include/propagation/bottomUpPropagationGraph.hpp
-  std::vector<VarId> variableStack_;
-  std::vector<InvariantId> invariantStack_;
-  std::vector<Timestamp> varStableAt;  // last timestamp when a VarID was stable
-                                       // (i.e., will not change)
-=======
   std::vector<Timestamp> varStableAt;  // last timestamp when a VarID was
                                        // stable (i.e., will not change)
->>>>>>> develop:include/propagation/bottomUpExplorer.hpp
   std::vector<Timestamp> invariantStableAt;
   std::vector<bool> varIsOnStack;
   std::vector<bool> invariantIsOnStack;
@@ -58,24 +45,9 @@ class BottomUpExplorer {
 
   void commitAndPostpone(Timestamp, VarId);
 
-<<<<<<< HEAD:include/propagation/bottomUpPropagationGraph.hpp
-  
-
- public:
-  // BottomUpPropagationGraph() : BottomUpPropagationGraph(1000) {}
-  BottomUpPropagationGraph(Engine& e, size_t expectedSize);
-
-  void fullPropagateAndCommit(Timestamp);
-
-  void propagate(Timestamp);
-
-  void propagate2(Timestamp);
-  void lazyPropagateAndCommit(Timestamp);
-=======
 public: 
   BottomUpExplorer() = delete;
   BottomUpExplorer(PropagationEngine& e, size_t expectedSize);
->>>>>>> develop:include/propagation/bottomUpExplorer.hpp
 
   void registerVar(VarId);
   void registerInvariant(InvariantId);
@@ -89,10 +61,6 @@ public:
   void propagate(Timestamp currentTime);
 };
 
-<<<<<<< HEAD:include/propagation/bottomUpPropagationGraph.hpp
-inline void BottomUpPropagationGraph::pushVariableStack(VarId v) {
-  assert(v.idType == VarIdType::var);
-=======
 inline void BottomUpExplorer::registerForPropagation(Timestamp, VarId id) {
   // TODO: why not set varIsOnStack.at(v) = true;?
   // I remember that there was some technical reason but this need to be
@@ -103,7 +71,6 @@ inline void BottomUpExplorer::registerForPropagation(Timestamp, VarId id) {
 inline void BottomUpExplorer::clearRegisteredVariables() { varStackIdx_ = 0; }
 
 inline void BottomUpExplorer::pushVariableStack(VarId v) {
->>>>>>> develop:include/propagation/bottomUpExplorer.hpp
   varIsOnStack.at(v) = true;
   variableStack_[varStackIdx_++] = v;
 }
@@ -129,21 +96,11 @@ inline InvariantId BottomUpExplorer::peekInvariantStack() {
   return invariantStack_[invariantStackIdx_ - 1];
 }
 
-<<<<<<< HEAD:include/propagation/bottomUpPropagationGraph.hpp
-inline void BottomUpPropagationGraph::markStable(Timestamp t, VarId v) {
-  assert(v.idType == VarIdType::var);
-  varStableAt.at(v) = t;
-}
-
-inline bool BottomUpPropagationGraph::isStable(Timestamp t, VarId v) {
-  assert(v.idType == VarIdType::var);
-=======
 inline void BottomUpExplorer::markStable(Timestamp t, VarId v) {
   varStableAt.at(v) = t;
 }
 
 inline bool BottomUpExplorer::isStable(Timestamp t, VarId v) {
->>>>>>> develop:include/propagation/bottomUpExplorer.hpp
   return varStableAt.at(v) == t;
 }
 
