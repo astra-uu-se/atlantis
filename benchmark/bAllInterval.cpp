1fad11033fa07e8d4c13ef7651ee3d15a24414f6
Gustav <gustav.bjordal@it.uu.se>
2020-10-22 17:17:07 +0200

    Added test for magicSquare and fixed bug

--- benchmark/bAllInterval.cpp
+++ benchmark/bAllInterval.cpp
@@ -60,8 +60,8 @@ BENCHMARK_DEFINE_F(AllInterval, probing_single_swap)(benchmark::State& st) {
   for (auto _ : st) {
     int i = distribution(gen);
     int j = distribution(gen);
-    Int oldI = engine->getValue(s_vars.at(i));
-    Int oldJ = engine->getValue(s_vars.at(j));
+    Int oldI = engine->getCommittedValue(s_vars.at(i));
+    Int oldJ = engine->getCommittedValue(s_vars.at(j));
     // Perform random swap
     engine->beginMove();
     engine->setValue(s_vars.at(i), oldJ);
@@ -79,8 +79,8 @@ BENCHMARK_DEFINE_F(AllInterval, probing_all_swap)(benchmark::State& st) {
   for (auto _ : st) {
     for (size_t i = 0; i < static_cast<size_t>(n); i++) {
       for (size_t j = i + 1; j < static_cast<size_t>(n); j++) {
-        Int oldI = engine->getValue(s_vars.at(i));
-        Int oldJ = engine->getValue(s_vars.at(j));
+        Int oldI = engine->getCommittedValue(s_vars.at(i));
+        Int oldJ = engine->getCommittedValue(s_vars.at(j));
         engine->beginMove();
         engine->setValue(s_vars.at(i), oldJ);
         engine->setValue(s_vars.at(j), oldI);
@@ -104,8 +104,8 @@ BENCHMARK_DEFINE_F(AllInterval, commit_single_swap)(benchmark::State& st) {
     int i = distribution(gen);
     int j = distribution(gen);
 
-    Int oldI = engine->getValue(s_vars.at(i));
-    Int oldJ = engine->getValue(s_vars.at(j));
+    Int oldI = engine->getCommittedValue(s_vars.at(i));
+    Int oldJ = engine->getCommittedValue(s_vars.at(j));
     // Perform random swap
     engine->beginMove();
     engine->setValue(s_vars.at(i), oldJ);
