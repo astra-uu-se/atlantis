e783cacfdb3a64548b91d0bd31d8c616165700b4
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2020-10-30 11:44:33 +0100

    improved message

--- slack-benchmark.sh
+++ slack-benchmark.sh
@@ -4,7 +4,7 @@ if [ ! -f "$BENCHMARK" ]; then
 fi
 BENCHMARK_DATA=`$BENCHMARK`
 BENCHMARK_FILE="benchmark-data.json"
-HEADER="`git config --get remote.origin.url | sed 's/[^\:]*\:\([^\.]*\)\.git/\1/'`@`git rev-parse --abbrev-ref HEAD` by `git show --quiet --pretty=format:%an`" > $BENCHMARK_FILE
+HEADER="Build #$TRAVIS_BUILD_NUMBER (`git branch -v | grep \* | sed 's/\S*\s*\S*\s\(\w*\).*/\1/'`) `git config --get remote.origin.url | sed 's/[^\.]*\.com[\:\/]*\([^\.]*\)\(\.git\)*\(@HEAD\)*/\1/'`@`git rev-parse --abbrev-ref HEAD` by `git show --quiet --pretty=format:%an`" > $BENCHMARK_FILE
 echo "{\"text\":\"${HEADER}\n\`\`\`${BENCHMARK_DATA}\`\`\`\"}" >> $BENCHMARK_FILE
 BENCHMARK_FILE_PATH=`realpath $BENCHMARK_FILE`
-curl -X POST -H 'Content-type: application/json' --data-binary "@$BENCHMARK_FILE_PATH" https://hooks.slack.com/services/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
+#curl -X POST -H 'Content-type: application/json' --data-binary "@$BENCHMARK_FILE_PATH" https://hooks.slack.com/services/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
