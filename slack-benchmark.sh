d0f86eec656a6f59c7976f0c189bef06812907b8
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2020-10-30 11:05:09 +0100

    Messaging slack with benchmark data

---
+++ slack-benchmark.sh
@@ -0,0 +1,10 @@
+BENCHMARK=./build/runBenchmarks
+if [ ! -f "$BENCHMARK" ]; then
+  exit 1
+fi
+BENCHMARK_DATA=`$BENCHMARK`
+BENCHMARK_FILE="benchmark-data.json"
+HEADER="`git config --get remote.origin.url | sed 's/[^\:]*\:\([^\.]*\)\.git/\1/'`@`git rev-parse --abbrev-ref HEAD` by `git show --quiet --pretty=format:%an`" > $BENCHMARK_FILE
+echo "{\"text\":\"${HEADER}\n\`\`\`${BENCHMARK_DATA}\`\`\`\"}" >> $BENCHMARK_FILE
+BENCHMARK_FILE_PATH=`realpath $BENCHMARK_FILE`
+curl -X POST -H 'Content-type: application/json' --data-binary "@$BENCHMARK_FILE_PATH" https://hooks.slack.com/services/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
