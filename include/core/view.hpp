22a4ffe458e0e9db2004492e0ac683be252ab6f7
Gustav <gustav.bjordal@it.uu.se>
2020-11-04 13:41:31 +0100

    renaming varView into just view

--- include/core/view.hpp
+++ include/core/view.hpp
@@ -2,15 +2,15 @@
 #include "core/types.hpp"
 #include "core/var.hpp"
 
-class VarView : public Var {
+class View : public Var {
  protected:
   VarId m_parentId;
 
  public:
-  VarView(const VarId t_parentId) : Var(NULL_ID), m_parentId(t_parentId) {
+  View(const VarId t_parentId) : Var(NULL_ID), m_parentId(t_parentId) {
     m_id.idType = VarIdType::view;
   }
-  virtual ~VarView() = default;
+  virtual ~View() = default;
   inline void setId(const VarId t_id) { m_id.id = t_id.id; }
   inline VarId getId() const { return m_id; };
   inline VarId getParentId() const { return m_parentId; }
