df1e3a5ae0b264b328ce017c5011a3d41b41fda2
Gustav <gustav.bjordal@it.uu.se>
2020-11-04 14:20:10 +0100

    nqueens

--- benchmark/bNQueens.cpp
+++ benchmark/bNQueens.cpp
@@ -2,16 +2,17 @@
 
 #include <constraints/allDifferent.hpp>
 #include <core/propagationEngine.hpp>
-#include <invariants/absDiff.hpp>
+#include <invariants/linear.hpp>
 #include <iostream>
 #include <random>
 #include <utility>
 #include <vector>
+#include <views/intOffsetView.hpp>
 
 class Queens : public benchmark::Fixture {
  public:
   std::unique_ptr<PropagationEngine> engine;
-  std::vector<VarId> q;
+  std::vector<VarId> queens;
   std::vector<VarId> q_offset_plus;
   std::vector<VarId> q_offset_minus;
   std::random_device rd;
@@ -20,28 +21,41 @@ class Queens : public benchmark::Fixture {
   std::uniform_int_distribution<> distribution;
   int n;
 
-  VarId violation = NULL_ID;
+  VarId violation1 = NULL_ID;
+  VarId violation2 = NULL_ID;
+  VarId violation3 = NULL_ID;
+  VarId total_violation = NULL_ID;
 
   void SetUp(const ::benchmark::State& state) {
     engine = std::make_unique<PropagationEngine>();
     n = state.range(0);
 
-    // TODO: why is this printed multiple times per test?
     // std::cout << n << "\n";
     engine->open();
 
     for (int i = 0; i < n; i++) {
-      s_vars.push_back(engine->makeIntVar(i, 0, n - 1));
+      VarId q = engine->makeIntVar(i, 0, n - 1);
+      queens.push_back(q);
+      q_offset_minus.push_back(
+          engine->makeIntView<IntOffsetView>(q, -i)->getId());
+      q_offset_plus.push_back(
+          engine->makeIntView<IntOffsetView>(q, i)->getId());
     }
 
-    for (int i = 1; i < n; i++) {
-      v_vars.push_back(engine->makeIntVar(i, 0, n - 1));
-      engine->makeInvariant<AbsDiff>(s_vars.at(i - 1), s_vars.at(i),
-                                     v_vars.back());
-    }
+    violation1 = engine->makeIntVar(0, 0, n);
+    violation2 = engine->makeIntVar(0, 0, n);
+    violation3 = engine->makeIntVar(0, 0, n);
 
-    violation = engine->makeIntVar(0, 0, n);
-    engine->makeConstraint<AllDifferent>(violation, v_vars);
+    engine->makeConstraint<AllDifferent>(violation1, queens);
+    engine->makeConstraint<AllDifferent>(violation2, q_offset_minus);
+    engine->makeConstraint<AllDifferent>(violation3, q_offset_plus);
+
+    total_violation = engine->makeIntVar(0, 0, 3 * n);
+
+    engine->makeInvariant<Linear>(
+        std::vector<Int>{1, 1, 1},
+        std::vector<VarId>{violation1, violation2, violation3},
+        total_violation);
 
     engine->close();
 
@@ -51,8 +65,9 @@ class Queens : public benchmark::Fixture {
   }
 
   void TearDown(const ::benchmark::State& state) {
-    s_vars.clear();
-    v_vars.clear();
+    queens.clear();
+    q_offset_minus.clear();
+    q_offset_plus.clear();
   }
 };
 
@@ -60,16 +75,16 @@ BENCHMARK_DEFINE_F(Queens, probing_single_swap)(benchmark::State& st) {
   for (auto _ : st) {
     int i = distribution(gen);
     int j = distribution(gen);
-    Int oldI = engine->getCommittedValue(s_vars.at(i));
-    Int oldJ = engine->getCommittedValue(s_vars.at(j));
+    Int oldI = engine->getCommittedValue(queens.at(i));
+    Int oldJ = engine->getCommittedValue(queens.at(j));
     // Perform random swap
     engine->beginMove();
-    engine->setValue(s_vars.at(i), oldJ);
-    engine->setValue(s_vars.at(j), oldI);
+    engine->setValue(queens.at(i), oldJ);
+    engine->setValue(queens.at(j), oldI);
     engine->endMove();
 
     engine->beginQuery();
-    engine->query(violation);
+    engine->query(total_violation);
     engine->endQuery();
   }
 }
@@ -79,15 +94,15 @@ BENCHMARK_DEFINE_F(Queens, probing_all_swap)(benchmark::State& st) {
   for (auto _ : st) {
     for (size_t i = 0; i < static_cast<size_t>(n); i++) {
       for (size_t j = i + 1; j < static_cast<size_t>(n); j++) {
-        Int oldI = engine->getCommittedValue(s_vars.at(i));
-        Int oldJ = engine->getCommittedValue(s_vars.at(j));
+        Int oldI = engine->getCommittedValue(queens.at(i));
+        Int oldJ = engine->getCommittedValue(queens.at(j));
         engine->beginMove();
-        engine->setValue(s_vars.at(i), oldJ);
-        engine->setValue(s_vars.at(j), oldI);
+        engine->setValue(queens.at(i), oldJ);
+        engine->setValue(queens.at(j), oldI);
         engine->endMove();
 
         engine->beginQuery();
-        engine->query(violation);
+        engine->query(total_violation);
         engine->endQuery();
 
         ++probes;
@@ -98,34 +113,9 @@ BENCHMARK_DEFINE_F(Queens, probing_all_swap)(benchmark::State& st) {
       benchmark::Counter(probes, benchmark::Counter::kIsRate);
 }
 
-BENCHMARK_DEFINE_F(Queens, commit_single_swap)(benchmark::State& st) {
-  int commits = 0;
-  for (auto _ : st) {
-    int i = distribution(gen);
-    int j = distribution(gen);
-
-    Int oldI = engine->getCommittedValue(s_vars.at(i));
-    Int oldJ = engine->getCommittedValue(s_vars.at(j));
-    // Perform random swap
-    engine->beginMove();
-    engine->setValue(s_vars.at(i), oldJ);
-    engine->setValue(s_vars.at(j), oldI);
-    engine->endMove();
-
-    engine->beginCommit();
-    engine->query(violation);
-    engine->endCommit();
-
-    ++commits;
-  }
-
-  st.counters["seconds_per_commit"] = benchmark::Counter(
-      commits, benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
-}
 //
 
 BENCHMARK_REGISTER_F(Queens, probing_single_swap)->DenseRange(5, 30, 5);
 BENCHMARK_REGISTER_F(Queens, probing_all_swap)
     ->Unit(benchmark::kMillisecond)
     ->DenseRange(5, 30, 5);
-BENCHMARK_REGISTER_F(Queens, commit_single_swap)->DenseRange(5, 30, 5);
