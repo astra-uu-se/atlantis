e98f76f3e8c1c93191805790ffe8a368a2fb4986
Gustav <gustav.bjordal@it.uu.se>
2020-11-12 11:36:52 +0100

    Revert "testing old notification scheme but using new api"
    This reverts commit d2987aa1167a9dc0ac77e1724b979ea4e7d15a99.

--- include/core/invariant.hpp
+++ include/core/invariant.hpp
@@ -112,9 +112,9 @@ class Invariant {
 
   virtual void notifyCurrentDependencyChanged(Timestamp, Engine& e) = 0;
 
-  virtual void notify(Timestamp t, Engine& e, LocalId id);
+  void notify(Timestamp t, Engine& e, LocalId id);
 
-  virtual void compute(Timestamp t, Engine& e);
+  void compute(Timestamp t, Engine& e);
 
   virtual void commit(Timestamp, Engine&) { m_isPostponed = false; };
   inline void postpone() { m_isPostponed = true; }
