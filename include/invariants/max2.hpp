43bab4ac3f95bb70be162fe2408f8edfd1b77a81
Samuel Bodin <samuel.bodin@live.se>
2021-03-04 15:36:26 +0100

    Added invariant template

--- include/invariants/max2.hpp
+++ include/invariants/max2.hpp
@@ -1,1 +1,29 @@
 #pragma once
+
+#include <memory>
+#include <vector>
+
+#include "../core/engine.hpp"
+#include "../core/intVar.hpp"
+#include "../core/invariant.hpp"
+#include "../core/tracer.hpp"
+#include "../core/types.hpp"
+
+/**
+ * Invariant for max(a,b) = c
+ *
+ */
+
+class Max2 : public Invariant {
+ private:
+  VarId m_a, m_b, m_c;
+
+ public:
+  Max2(VarId a, VarId b, VarId c);
+  void init(Timestamp, Engine&) override;
+  void recompute(Timestamp, Engine&) override;
+  void notifyIntChanged(Timestamp t, Engine& e, LocalId id) override;
+  VarId getNextDependency(Timestamp, Engine&) override;
+  void notifyCurrentDependencyChanged(Timestamp, Engine& e) override;
+  void commit(Timestamp, Engine&) override;
+};
