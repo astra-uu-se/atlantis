7d246f4e68a3b0ce732d263ec8b1b97bf6ef7b5b
Max Perea Düring <maxpereaduring@gmail.com>
2021-03-05 15:53:15 +0100

    variable maps.

---
+++ src/fznparser/src/maps.cpp
@@ -0,0 +1,39 @@
+#include "maps.hpp"
+
+/*******************VARIABLEMAP****************************/
+Variable* VariableMap::add(std::shared_ptr<Variable> variable) {
+  if (!exists(variable->getName())) {
+    _variables.insert(std::pair<std::string, std::shared_ptr<Variable>>(
+        variable->getName(), variable));
+    _variableArray.push_back(variable.get());
+  }
+  return find(variable->getName());
+}
+Variable* VariableMap::find(const std::string name) const {
+  assert(_variables.find(name) != _variables.end());
+  return _variables.find(name)->second.get();
+}
+bool VariableMap::exists(std::string name) {
+  return (_variables.find(name) != _variables.end());
+}
+std::vector<Variable*> VariableMap::getArray() { return _variableArray; }
+
+/****************************CONSTRAINTMAP**********************/
+
+void ConstraintMap::add(std::shared_ptr<Constraint> constraint) {
+  assert(!exists(constraint.get()));
+  _constraints.insert(std::pair<Constraint*, std::shared_ptr<Constraint>>(
+      constraint.get(), constraint));
+  _constraintArray.push_back(constraint.get());
+}
+bool ConstraintMap::exists(Constraint* constraint) {
+  return (_constraints.find(constraint) != _constraints.end());
+}
+void ConstraintMap::remove(Constraint* constraint) {
+  assert(exists(constraint));
+  _constraints.erase(constraint);
+  auto it =
+      std::find(_constraintArray.begin(), _constraintArray.end(), constraint);
+  _constraintArray.erase(it);
+}
+std::vector<Constraint*> ConstraintMap::getArray() { return _constraintArray; }
---
+++ src/fznparser/src/maps.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <algorithm>
+
+#include "structure.hpp"
+#include "variable.hpp"
+
+class Variable;
+
+class VariableMap {
+ public:
+  VariableMap() = default;
+  virtual ~VariableMap() = default;
+  Variable* add(std::shared_ptr<Variable> variable);
+  Variable* find(std::string name) const;
+  bool exists(std::string name);
+  std::vector<Variable*> getArray();
+
+ private:
+  std::vector<Variable*> _variableArray;
+  std::map<std::string, std::shared_ptr<Variable>> _variables;
+};
+
+class ConstraintMap {
+ public:
+  ConstraintMap() = default;
+  virtual ~ConstraintMap() = default;
+  void add(std::shared_ptr<Constraint> constraint);
+  bool exists(Constraint* constraint);
+  void remove(Constraint* constraint);
+  std::vector<Constraint*> getArray();
+
+ private:
+  std::vector<Constraint*> _constraintArray;
+  std::map<Constraint*, std::shared_ptr<Constraint>> _constraints;
+};
