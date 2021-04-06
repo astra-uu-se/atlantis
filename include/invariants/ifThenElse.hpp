da4c803139cbe43e4cdb55cfc56126f8ed639d28
Samuel Bodin <samuel.bodin@live.se>
2021-04-06 11:57:06 +0200

    Reorder constructor args as per suggestion

--- include/invariants/ifThenElse.hpp
+++ include/invariants/ifThenElse.hpp
@@ -12,21 +12,22 @@
 // NOTE:
 // This is Python syntax and does not come naturally to me
 // Seeing IfThenElse(x1, x2, x3, x4) I would assume that
-// It would represent x4 = (if x1 then x2 else x3) (as the name of 
+// It would represent x4 = (if x1 then x2 else x3) (as the name of
 // invariant suggests). I strongly propone reordering of input
 // variables
 /**
- * Invariant for z <- x if b = 0 else y.
+ * Invariant for z <- if b = 0 then x else y
  *
  */
 
 class IfThenElse : public Invariant {
  private:
+  VarId m_b;
   std::vector<VarId> m_xy;
-  VarId m_b, m_z;
+  VarId m_z;
 
  public:
-  IfThenElse(VarId x, VarId y, VarId b, VarId z);
+  IfThenElse(VarId b, VarId x, VarId y, VarId z);
   void init(Timestamp, Engine&) override;
   void recompute(Timestamp, Engine&) override;
   void notifyIntChanged(Timestamp t, Engine& e, LocalId id) override;
