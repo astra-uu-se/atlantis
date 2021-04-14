708f708f72c2a8b60b32b40cb80ec9948654210f
Samuel Bodin <samuel.bodin@live.se>
2021-04-14 14:54:59 +0200

    Naive implementation of 'Or'. Used temporarily in benchmarking VesselLoading until MinSparse is tested

---
+++ include/constraints/or.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "../core/constraint.hpp"
+#include "../core/engine.hpp"
+#include "../core/intVar.hpp"
+#include "../core/tracer.hpp"
+#include "../core/types.hpp"
+
+class Or : public Constraint {
+ private:
+  std::vector<VarId> m_X;
+
+ public:
+  Or(VarId violationId, std::vector<VarId> X);
+
+  void init(Timestamp, Engine&) override;
+  void recompute(Timestamp, Engine&) override;
+  void notifyIntChanged(Timestamp t, Engine& e, LocalId id) override;
+  void commit(Timestamp, Engine&) override;
+  VarId getNextDependency(Timestamp, Engine&) override;
+  void notifyCurrentDependencyChanged(Timestamp, Engine& e) override;
+};
---
+++ src/constraints/or.cpp
@@ -0,0 +1,69 @@
+#include "constraints/or.hpp"
+#include <utility>
+
+// Temporary naive implementation that recomputes min(X).
+// Currently used in VesselLoading benchmark.
+// Use invariant/MinSparse instead once it has been tested
+
+Or::Or(VarId violationId, std::vector<VarId> X)
+    : Constraint(NULL_ID, violationId),
+      m_X(std::move(X)) {
+  m_modifiedVars.reserve(m_X.size());
+}
+
+void Or::init(Timestamp, Engine& e) {
+  assert(m_id != NULL_ID);
+  registerDefinedVariable(e, m_violationId);
+  for (size_t i = 0; i < m_X.size(); ++i) {
+    e.registerInvariantDependsOnVar(m_id, m_X[i], LocalId(0));
+  }
+}
+
+void Or::recompute(Timestamp t, Engine& e) {
+  Int min = e.getValue(t, m_X.front());
+  for (size_t i = 1; i < m_X.size(); ++i) {
+    min = std::min(min, e.getValue(t, m_X[i]));
+  }
+  updateValue(t, e, m_violationId, min);
+}
+
+// Temporary
+void Or::notifyIntChanged(Timestamp t, Engine& e, LocalId i) {
+  auto newValue = e.getValue(t, m_X[i]);
+  if (newValue < e.getValue(t, m_violationId)) {
+    updateValue(t, e, m_violationId, newValue);
+  } else {
+    Int min = e.getValue(t, m_X.front());
+    for (size_t j = 1; j < m_X.size(); ++j) {
+      min = std::min(min, e.getValue(t, m_X[j]));
+    }
+    updateValue(t, e, m_violationId, min);
+  }
+}
+
+VarId Or::getNextDependency(Timestamp t, Engine&) {
+  m_state.incValue(t, 1);
+  if (static_cast<size_t>(m_state.getValue(t)) == m_X.size()) {
+    return NULL_ID;  // Done
+  } else {
+    return m_X.at(m_state.getValue(t));
+  }
+}
+
+void Or::notifyCurrentDependencyChanged(Timestamp t, Engine& e) {
+  assert(m_state.getValue(t) != -1);
+  Int idx = m_state.getValue(t);
+  auto newValue = e.getValue(t, m_X[idx]);
+
+  if (newValue < e.getValue(t, m_violationId)) {
+    incValue(t, e, m_violationId, newValue);
+  } else {
+    Int min = e.getValue(t, m_X.front());
+    for (size_t i = 1; i < m_X.size(); ++i) {
+      min = std::min(min, e.getValue(t, m_X[i]));
+    }
+    updateValue(t, e, m_violationId, min);
+  }
+}
+
+void Or::commit(Timestamp t, Engine& e) { Invariant::commit(t, e); }
