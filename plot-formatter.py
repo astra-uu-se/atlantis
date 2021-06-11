3d3f994ea12ebff71add91cd05a42c108f76b99c
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2021-06-11 12:54:39 +0200

    updated plotter

--- plot-formatter.py
+++ plot-formatter.py
@@ -10,46 +10,65 @@ import json
 
 def int_to_propagation_mode(value):
     if value == 0:
-        return "i2o"
+        return 'i2o'
     elif value == 1:
-        return "mixed"
+        return 'mixed'
     elif value == 2:
-        return "o2i"
-    return ""
+        return 'o2i'
+    return ''
 
 
 def propagation_mode_to_marker(mode):
-    if mode == "i2o":
-        return "o"
-    elif mode == "mixed":
-        return "v"
-    elif mode == "o2i":
-        return "*"
-    return ""
+    if mode == 'i2o':
+        return 'o'
+    elif mode == 'mixed':
+        return 'v'
+    elif mode == 'o2i':
+        return '*'
+    return ''
 
 
 class PlotFormatter:
     def __init__(self, arguments):
-        self.arguments = arguments
+        self.arguments = self.process_arguments(arguments)
         self.logger = logging.getLogger('PlotFormatter')
         
         json_data = self.retrieve_json()
         self.benchmarks = self.retrieve_benchmarks(json_data)
 
+    def process_arguments(self, arguments):
+        json_file_path = arguments.get('input', None)
+        if json_file_path is None or not path.isfile(json_file_path):
+            raise Exception("JSON input file does not exists")
+
+        if arguments.get('save_plots') != True:
+            return arguments
+
+        if arguments.get('output_dir', None) is None or not path.isdir(arguments['output_dir']):
+            raise Exception("Output directory does not exist")
+        
+        if len(arguments.get('file_prefix', '')) > 0:
+            arguments['file_prefix'] = arguments['file_prefix'] + '-'
+        else:
+            arguments['file_prefix'] = ''
+
+        if len(arguments.get('file_suffix', '')) > 0:
+            arguments['file_suffix'] = '-' + arguments['file_suffix']
+        else:
+            arguments['file_suffix'] = ''
+        
+        return arguments
+
     def retrieve_json(self):
-        json_file_path = self.arguments.get('input', None)
-        if json_file_path is None:
-            return dict()
-        if not path.isfile(json_file_path):
-            return dict()
-        with open(json_file_path, 'r') as json_file:
+        with open(self.arguments['input'], 'r') as json_file:
             json_data = json.load(json_file)
         return json_data
 
     def retrieve_benchmarks(self, json_data):
         benchmarks = json_data.get('benchmarks', None)
         if benchmarks is None:
-            return list()
+            return dict()
+
         processed_benchmarks = dict()
         for benchmark in benchmarks:
             probes_per_second = benchmark.get('probes_per_second', None)
@@ -79,7 +98,6 @@ class PlotFormatter:
         return processed_benchmarks
 
     def plot_benchmarks(self):
-        
         for problem_name, models in self.benchmarks.items():
 
             for model_name, modes in models.items():
@@ -102,19 +120,39 @@ class PlotFormatter:
                 ticks.sort()
                 plt.xticks(ticks)
                 plt.legend()
-                plt.show()
+                if self.arguments.get('save_plots', False) != True:
+                    plt.show()
+                else:
+                    plot_filename = path.join(
+                        self.arguments['output_dir'],
+                        f'{self.arguments["file_prefix"]}{problem_name}-{model_name}{self.arguments["file_suffix"]}.png'
+                    )
+                    plt.savefig(plot_filename)
+                    plt.clf()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING)
-    flags = ["input"]
-    arguments = {f:None for f in flags}
-    for f in flags:
+
+    arguments = {
+        # key: default value
+        'input': 'tmp.json',
+        'output_dir': 'plots',
+        'file_prefix': '',
+        'file_suffix': ''
+    }
+    bool_flags = ['save_plots']
+
+    for f in arguments.keys():
         flag = '--' + f +'='
         argument = next((n[len(flag):] for n in argv if n.startswith(flag)), None)
-        arguments[f] = argument
-    if arguments.get('input', None) is None:
-        arguments['input'] = 'tmp.json'
+        if argument is not None:
+            arguments[f] = argument
     
+    for f in bool_flags:
+        flag = '--' + f
+        argument = any((n.startswith(flag) for n in argv))
+        arguments[f] = argument
+
     plot_formatter = PlotFormatter(arguments)
     plot_formatter.plot_benchmarks()
