17ade727e4743d924eebfe0fe4685984c977eb96
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2021-12-03 11:03:54 +0100

    removed non-plot modified files from branch
    Removed non-modified files from branch that are not part of the python plotter: they will possibly become their own pull requests

--- test/core/tPriorityList.cpp
+++ test/core/tPriorityList.cpp
@@ -17,12 +17,6 @@ class PriorityListTest : public ::testing::Test {
   }
   std::mt19937 gen;
 
-  void testSanity([[maybe_unused]] PriorityList &p,
-                  [[maybe_unused]] Timestamp t) {
-#ifndef NDEBUG
-    p.sanity(t);
-#endif
-  }
   void updateForward(Timestamp ts, PriorityList &priorityList) {
     updateForward(ts, priorityList, 0);
   }
@@ -38,14 +32,12 @@ class PriorityListTest : public ::testing::Test {
   void updateBackwards(Timestamp ts, PriorityList &priorityList, Int offset) {
     for (size_t idx = 0; idx < priorityList.size(); ++idx) {
       priorityList.updatePriority(ts, idx, priorityList.size() - idx + offset);
-      testSanity(priorityList, ts);
     }
   }
 
   void updateUniform(Timestamp ts, PriorityList &priorityList) {
     for (size_t idx = 0; idx < priorityList.size(); ++idx) {
       priorityList.updatePriority(ts, idx, Int(ts));
-      testSanity(priorityList, ts);
     }
   }
 };
@@ -58,7 +50,6 @@ TEST_F(PriorityListTest, Constructor) {
   for (size_t size = 0; size < 100; ++size) {
     PriorityList priorityList(size);
     EXPECT_EQ(priorityList.size(), size);
-    testSanity(priorityList, 0);
   }
 }
 
@@ -114,7 +105,6 @@ TEST_F(PriorityListTest, RandomUpdatePriority) {
       minPriority = std::min(newValue, minPriority);
       maxPriority = std::max(newValue, maxPriority);
       priorityList.updatePriority(ts, idx, newValue);
-      testSanity(priorityList, ts);
     }
 
     EXPECT_EQ(priorityList.getMinPriority(ts), minPriority);
