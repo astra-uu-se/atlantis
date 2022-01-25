d297dffb2c8c2557d0aeb60ae21e1474cd8d924c
Maarten Flippo <maartenflippo@outlook.com>
2022-01-25 11:30:36 +0100

    Completed the benchmark to include random assignment.

--- benchmark/bSudoku.cpp
+++ benchmark/bSudoku.cpp
@@ -38,14 +38,23 @@ class Sudoku : public benchmark::Fixture {
   std::vector<std::vector<VarId>> sudokuRows;  // The rows of the sudoku.
   std::vector<std::vector<VarId>> sudokuCols;  // The columns of the sudoku.
 
-  std::vector<std::tuple<int, int>> freeCells;  // Which row,col cells are free.
+  // Which row,col cells are free, indexed by the block they belong to.
+  std::array<std::vector<std::tuple<int, int>>, SIZE> freeCells;
+  // What values are missing in each block. These are the values to pick from
+  // when assigning values to variables.
+  std::array<std::set<Int>, SIZE> missingValues;
+  // Contains the possible values to pick from for each block. It differs from
+  // missingValues because this will be mutated during the construction of the
+  // initial assignment to ensure no duplicate values are assigned to the same
+  // block.
+  std::array<std::set<Int>, SIZE> possibleValues;
 
   std::vector<VarId> allDiffViolations;
   VarId totalViolations;
 
   std::random_device rd;
   std::mt19937 gen;
-  std::uniform_int_distribution<> distr;
+  std::uniform_int_distribution<> blockDistr;
 
   void SetUp(const ::benchmark::State& state) {
     engine = std::make_unique<PropagationEngine>();
@@ -70,7 +79,6 @@ class Sudoku : public benchmark::Fixture {
     int sudokuId = state.range(1);
     sudokuRows.resize(SIZE);
     sudokuCols.resize(SIZE);
-    freeCells.reserve(SIZE * SIZE);
     allDiffViolations.reserve(2 * SIZE);
 
     for (int i = 0; i < static_cast<int>(SIZE); i++) {
@@ -78,29 +86,38 @@ class Sudoku : public benchmark::Fixture {
       sudokuRows[i].resize(SIZE);
     }
 
+    computeMissingValues(sudokuId);
+    std::copy(missingValues.begin(), missingValues.end(),
+              possibleValues.begin());
+
     // Setup the variables for the cells of the sudoku.
     for (int row = 0; row < static_cast<int>(SIZE); row++) {
       for (int col = 0; col < static_cast<int>(SIZE); col++) {
+        int block = (row - (row % SQRT_SIZE)) + col / SQRT_SIZE;
+
         if (SUDOKUS[sudokuId][row][col] == 0) {
-          freeCells.push_back(std::make_tuple(row, col));
+          freeCells[block].push_back(std::make_tuple(row, col));
         }
 
-        Int upperbound = SUDOKUS[sudokuId][row][col] == 0
-                             ? SIZE
-                             : SUDOKUS[sudokuId][row][col];
+        Int initialValue = SUDOKUS[sudokuId][row][col];
+        if (initialValue == 0) {
+          initialValue = pullAvailableValue(block);
+        }
 
-        Int lowerbound =
-            SUDOKUS[sudokuId][row][col] == 0 ? 1 : SUDOKUS[sudokuId][row][col];
+        assert(1 <= initialValue && initialValue <= static_cast<Int>(SIZE));
 
-        int initialValue = 1;  // We don't care about solving for now
-
-        auto cellVariable =
-            engine->makeIntVar(initialValue, lowerbound, upperbound);
+        auto cellVariable = engine->makeIntVar(initialValue, 1, SIZE);
 
         sudokuRows[row][col] = cellVariable;
         sudokuCols[col][row] = cellVariable;
       }
     }
+
+#ifndef NDEBUG
+    for (size_t block = 0; block < SIZE; block++) {
+      assert(possibleValues[block].empty());
+    }
+#endif
 
     // Setup the violation variables.
     for (int i = 0; i < static_cast<int>(SIZE); i++) {
@@ -118,22 +135,72 @@ class Sudoku : public benchmark::Fixture {
 
     engine->close();
 
-    distr =
-        std::uniform_int_distribution<>{0, static_cast<int>(freeCells.size())};
+    blockDistr = std::uniform_int_distribution<>{0, static_cast<int>(SIZE) - 1};
   }
 
   void TearDown(const ::benchmark::State&) {
     allDiffViolations.clear();
     sudokuRows.clear();
     sudokuCols.clear();
-    freeCells.clear();
+
+    std::for_each(freeCells.begin(), freeCells.end(),
+                  [](auto& vec) { vec.clear(); });
+  }
+
+ private:
+  Int pullAvailableValue(int blockId) {
+    assert(!possibleValues[blockId].empty());
+
+    size_t originalSize = possibleValues[blockId].size();
+    std::uniform_int_distribution idxDistr(
+        0, static_cast<int>(possibleValues[blockId].size()) - 1);
+
+    std::set<Int>::const_iterator it = possibleValues[blockId].cbegin();
+    int offset = idxDistr(gen);
+    std::advance(it, offset);
+
+    Int value = *it;
+    possibleValues[blockId].erase(value);
+
+    size_t newSize = possibleValues[blockId].size();
+    assert(originalSize - newSize == 1);
+
+    return value;
+  }
+
+  void computeMissingValues(int sudokuId) {
+    for (size_t block = 0; block < SIZE; block++) {
+      missingValues[block] = std::set<Int>();
+      for (Int value = 1; value <= static_cast<Int>(SIZE); value++) {
+        missingValues[block].insert(value);
+      }
+    }
+
+    for (size_t block = 0; block < SIZE; block++) {
+      for (size_t row = 0; row < SQRT_SIZE; row++) {
+        for (size_t col = 0; col < SQRT_SIZE; col++) {
+          int global_row = (block / SQRT_SIZE) * SQRT_SIZE + row;
+          int global_col = (block % SQRT_SIZE) * SQRT_SIZE + col;
+          Int value = SUDOKUS[sudokuId][global_row][global_col];
+
+          if (value > 0) {
+            missingValues[block].erase(value);
+          }
+        }
+      }
+    }
   }
 };
 
 BENCHMARK_DEFINE_F(Sudoku, probing_single_swap)(benchmark::State& st) {
   for (auto _ : st) {
-    std::tuple<int, int> iPos = freeCells[distr(gen)];
-    std::tuple<int, int> jPos = freeCells[distr(gen)];
+    size_t block = static_cast<size_t>(blockDistr(gen));
+
+    std::uniform_int_distribution idxDistr(
+        0, static_cast<int>(freeCells[block].size()) - 1);
+
+    std::tuple<int, int> iPos = freeCells[block][idxDistr(gen)];
+    std::tuple<int, int> jPos = freeCells[block][idxDistr(gen)];
 
     VarId i = sudokuRows[std::get<0>(iPos)][std::get<1>(iPos)];
     VarId j = sudokuRows[std::get<0>(jPos)][std::get<1>(jPos)];
