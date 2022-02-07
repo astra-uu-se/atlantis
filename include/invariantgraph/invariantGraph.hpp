852c54952fca0a23f0577671c69337bcd52d5704
Maarten Flippo <maartenflippo@outlook.com>
2022-02-07 09:09:10 +0100

    Start invariant structure application on the engine.

---
+++ include/invariantgraph/invariantGraph.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <set>
+#include <vector>
+
+#include "core/engine.hpp"
+#include "structure.hpp"
+
+namespace invariantgraph {
+class InvariantGraph {
+ private:
+  std::set<std::shared_ptr<InvariantNode>> appliedInvariants;
+  std::set<std::shared_ptr<SoftConstraintNode>> appliedSoftConstraints;
+  std::map<std::shared_ptr<VariableNode>, VarId> engineVariables;
+  std::set<std::shared_ptr<VariableNode>> violationVars;
+
+ public:
+  void apply(Engine& engine);
+
+ private:
+  void applyVariable(Engine& engine, const std::shared_ptr<VariableNode>& node);
+  void applyInvariant(Engine& engine,
+                      const std::shared_ptr<InvariantNode>& node);
+  void applyConstraint(Engine& engine,
+                       const std::shared_ptr<SoftConstraintNode>& node);
+
+  [[nodiscard]] Int totalViolationsUpperBound(Engine& engine) const;
+  [[nodiscard]] std::vector<VarId> getViolationVariables() const;
+
+  [[nodiscard]] bool wasVisited(
+      const std::shared_ptr<VariableNode>& node) const {
+    return engineVariables.count(node) > 0;
+  }
+
+  [[nodiscard]] bool wasVisited(
+      const std::shared_ptr<InvariantNode>& node) const {
+    return appliedInvariants.count(node) > 0;
+  }
+
+  [[nodiscard]] bool wasVisited(
+      const std::shared_ptr<SoftConstraintNode>& node) const {
+    return appliedSoftConstraints.count(node) > 0;
+  }
+};
+}  // namespace invariantgraph
