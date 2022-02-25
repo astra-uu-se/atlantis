e64047a4d022732b90cfcbadce3c0c682abdbbb4
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2022-02-25 10:27:35 +0100

    refactored o2i explorer
    stable became computed
    isUpToDate became isMarked (bool value was negated with this refactoring)

--- benchmark/benchmark.hpp
+++ benchmark/benchmark.hpp
@@ -5,23 +5,25 @@
 
 inline PropagationMode intToPropagationMode(int state) {
   switch (state) {
-    case 0:
-      return PropagationMode::INPUT_TO_OUTPUT;
-    case 1:
-    case 2:
     case 3:
+    case 2:
+    case 1:
       return PropagationMode::OUTPUT_TO_INPUT;
+    case 0:
+    default:
+      return PropagationMode::INPUT_TO_OUTPUT;
   }
 }
 
 inline OutputToInputMarkingMode intToOutputToInputMarkingMode(int state) {
   switch (state) {
-    case 0:
-    case 1:
-      return OutputToInputMarkingMode::NONE;
-    case 2:
-      return OutputToInputMarkingMode::MARK_SWEEP;
     case 3:
       return OutputToInputMarkingMode::TOPOLOGICAL_SORT;
+    case 2:
+      return OutputToInputMarkingMode::MARK_SWEEP;
+    case 1:
+    case 0:
+    default:
+      return OutputToInputMarkingMode::NONE;
   }
 }
