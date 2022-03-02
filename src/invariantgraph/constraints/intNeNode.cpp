870f4246bf3391ecba3d92b3b6ed99e6c91e9d83
Maarten Flippo <maartenflippo@outlook.com>
2022-03-02 11:33:51 +0100

    Fix naming.

--- src/invariantgraph/constraints/intNeNode.cpp
+++ src/invariantgraph/constraints/intNeNode.cpp
@@ -1,10 +1,10 @@
-#include "invariantgraph/constraints/intNeqNode.hpp"
+#include "invariantgraph/constraints/intNeNode.hpp"
 
 #include "constraints/notEqual.hpp"
 #include "invariantgraph/parseHelper.hpp"
 
-std::unique_ptr<invariantgraph::IntNeqNode>
-invariantgraph::IntNeqNode::fromModelConstraint(
+std::unique_ptr<invariantgraph::IntNeNode>
+invariantgraph::IntNeNode::fromModelConstraint(
     const std::shared_ptr<fznparser::Constraint>& constraint,
     const std::function<VariableNode*(std::shared_ptr<fznparser::Variable>)>&
         variableMap) {
@@ -14,14 +14,14 @@ invariantgraph::IntNeqNode::fromModelConstraint(
   MAPPED_SEARCH_VARIABLE_ARG(a, constraint->arguments()[0], variableMap);
   MAPPED_SEARCH_VARIABLE_ARG(b, constraint->arguments()[1], variableMap);
 
-  auto node = std::make_unique<IntNeqNode>(a, b);
+  auto node = std::make_unique<IntNeNode>(a, b);
   a->addSoftConstraint(node.get());
   b->addSoftConstraint(node.get());
 
   return node;
 }
 
-VarId invariantgraph::IntNeqNode::registerWithEngine(
+VarId invariantgraph::IntNeNode::registerWithEngine(
     Engine& engine, std::function<VarId(VariableNode*)> variableMapper) const {
   VarId violation = engine.makeIntVar(0, 0, 1);
 
