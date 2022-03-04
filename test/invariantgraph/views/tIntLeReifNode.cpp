fbbdc67088512dcef1cafb440c3f557a3fed9a3d
Maarten Flippo <maartenflippo@outlook.com>
2022-03-04 11:49:24 +0100

    Implement identification of int_le_reif.

---
+++ test/invariantgraph/views/tIntLeReifNode.cpp
@@ -0,0 +1,46 @@
+#include "../nodeTestBase.hpp"
+#include "core/propagationEngine.hpp"
+#include "invariantgraph/views/intLeReifNode.hpp"
+
+class IntLeReifNodeTest : public NodeTestBase {
+ public:
+  std::shared_ptr<fznparser::SearchVariable> a;
+  std::shared_ptr<fznparser::SearchVariable> b;
+  std::shared_ptr<fznparser::SearchVariable> r;
+
+  std::unique_ptr<invariantgraph::IntLeReifNode> node;
+
+  void SetUp() override {
+    a = FZN_SEARCH_VARIABLE("a", 5, 10);
+    b = FZN_SEARCH_VARIABLE("b", 2, 7);
+    r = FZN_SEARCH_VARIABLE("r", 0, 1);
+
+    auto constraint = makeConstraint("int_le_reif", FZN_NO_ANNOTATIONS, a, b, r);
+
+    node = makeNode<invariantgraph::IntLeReifNode>(constraint);
+  }
+};
+
+TEST_F(IntLeReifNodeTest, construction) {
+  EXPECT_EQ(node->variable(), r);
+}
+
+TEST_F(IntLeReifNodeTest, application) {
+  std::map<invariantgraph::VariableNode*, VarId> variableMap;
+
+  PropagationEngine engine;
+  engine.open();
+  for (const auto& var : _variables)
+    var->registerWithEngine(engine, variableMap);
+  node->registerWithEngine(engine, variableMap);
+  engine.close();
+
+  // a, b
+  EXPECT_EQ(engine.getDecisionVariables().size(), 2);
+
+  // a, b and r
+  EXPECT_EQ(engine.getNumVariables(), 3);
+
+  // a, b and r
+  EXPECT_EQ(variableMap.size(), 3);
+}
