684203119813ae4a58aa1bd8abace81772be30c7
Maarten Flippo <maartenflippo@outlook.com>
2022-03-07 11:53:43 +0100

    Rework assignment with tests.

--- include/search/assignment.hpp
+++ include/search/assignment.hpp
@@ -1,48 +1,72 @@
 #pragma once
 
+#include <functional>
+
+#include "core/propagationEngine.hpp"
+
 namespace search {
 
-class Cost;
-class Move;
-class Neighbourhood;
+class AssignmentModification {
+ private:
+  PropagationEngine& _engine;
+
+ public:
+  explicit AssignmentModification(PropagationEngine& engine) : _engine(engine) {
+    assert(engine.isMoving());
+  }
+
+  /**
+   * Assign a value to a variable. Overrides any modifications previously made
+   * to @p var.
+   *
+   * @param var The variable to assign.
+   * @param value The value to assign to the variable.
+   */
+  void set(VarId var, Int value) {
+    _engine.setValue(var, value);
+  }
+};
 
 class Assignment {
+ private:
+  PropagationEngine& _engine;
+  VarId& _violation;
+  VarId& _objective;
+
  public:
-  /**
-   * Initialises all the decision variables. The given neighbourhood should
-   * define all decision variables at initialisation.
-   *
-   * @param neighbourhood The neighbourhood used to initialise all the decision
-   * variables.
-   */
-  void initialise(Neighbourhood* neighbourhood);
+  explicit Assignment(PropagationEngine& engine, VarId& violation, VarId& objective)
+      : _engine(engine), _violation(violation), _objective(objective) {}
 
   /**
-   * Determine the cost of a move. Will mutate @p move to indicate it has been
-   * probed.
+   * Assign values to the variables in the assignment. This is supplied a
+   * callback, which receives a @p AssignmentModification instance, through
+   * which the variables can be changed. For example:
    *
-   * @param move The move to probe.
+   *     Assignment assignment;
+   *     assignment.assign([&](auto& modifications) {
+   *       modifications.set(a, 1);
+   *       modifications.set(b, 2);
+   *     });
+   *
+   * @param modificationFunc The callback which sets the variables and their
+   * new values.
    */
-  void probeMove(Move& move);
+  void assign(
+      const std::function<void(AssignmentModification&)>& modificationFunc);
 
   /**
-   * Change the assignment according to @p move. This will be reflected in
-   * the cost of the assignment.
+   * Get the value of a variable in the current assignment.
    *
-   * @param move The move to commit.
+   * @param var The variable for which to query the value.
+   * @return The value of @p var.
    */
-  void commitMove(const Move& move);
+  Int value(VarId var);
 
   /**
    * @return True if the current assignment satisfies all the constraints, false
    * otherwise.
    */
-  bool satisfiesConstraints() const noexcept;
-
-  /**
-   * @return The cost of the current assignment.
-   */
-  Cost cost() const noexcept;
+  bool satisfiesConstraints();
 };
 
 }  // namespace search
