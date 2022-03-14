9f3b828b6406b7ac6f059ed2268fc42f0171d77a
Maarten Flippo <maartenflippo@outlook.com>
2022-03-14 11:23:42 +0100

    Change signature of Assignment::probe and Assignment::assign.
    Previously they both took a std::function, however, this describes an owning function pointer. It might also allocate on the heap dynamically if too many variables are captured in the lambda.
    
    Using a templated parameter type eliminates this cost, whilst being only slightly slower to compile.

--- src/search/assignment.cpp
+++ src/search/assignment.cpp
@@ -4,27 +4,6 @@ Int search::Assignment::value(VarId var) const noexcept {
   return _engine.getCommittedValue(var);
 }
 
-void search::Assignment::assign(
-    const std::function<void(AssignmentModification &)> &modificationFunc) {
-  move(modificationFunc);
-
-  _engine.beginCommit();
-  _engine.query(_objective);
-  _engine.endCommit();
-}
-
-search::Cost search::Assignment::probe(
-    const std::function<void(AssignmentModification &)> &modificationFunc) {
-  move(modificationFunc);
-
-  _engine.beginQuery();
-  _engine.query(_objective);
-  _engine.query(_violation);
-  _engine.endQuery();
-
-  return {_engine.getNewValue(_violation), _engine.getNewValue(_objective)};
-}
-
 bool search::Assignment::satisfiesConstraints() const noexcept {
   return _engine.getCommittedValue(_violation) == 0;
 }
@@ -32,12 +11,4 @@ bool search::Assignment::satisfiesConstraints() const noexcept {
 search::Cost search::Assignment::cost() const noexcept {
   return {_engine.getCommittedValue(_violation),
           _engine.getCommittedValue(_objective)};
-}
-
-void search::Assignment::move(
-    const std::function<void(AssignmentModification &)> &modificationFunc) {
-  _engine.beginMove();
-  AssignmentModification modifications(_engine);
-  modificationFunc(modifications);
-  _engine.endMove();
 }
