3b3b4927848c2cd469a524163c80eeb747112f78
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2022-03-16 14:02:12 +0100

    fixed merge issue

--- src/search/assignment.cpp
+++ src/search/assignment.cpp
@@ -1,14 +1,14 @@
 #include "search/assignment.hpp"
 
 Int search::Assignment::value(VarId var) const noexcept {
-  return _engine.getCommittedValue(var);
+  return _engine.committedValue(var);
 }
 
 bool search::Assignment::satisfiesConstraints() const noexcept {
-  return _engine.getCommittedValue(_violation) == 0;
+  return _engine.committedValue(_violation) == 0;
 }
 
 search::Cost search::Assignment::cost() const noexcept {
-  return {_engine.getCommittedValue(_violation),
-          _engine.getCommittedValue(_objective)};
+  return {_engine.committedValue(_violation),
+          _engine.committedValue(_objective)};
 }
