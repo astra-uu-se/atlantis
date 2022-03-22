b5ab1878d7e2a16bf0f0e2095bb3283c51f1fb9e
Maarten Flippo <maartenflippo@outlook.com>
2022-03-22 15:18:23 +0100

    Pull out macros for better reuse.

--- test/invariantgraph/nodeTestBase.hpp
+++ test/invariantgraph/nodeTestBase.hpp
@@ -4,6 +4,7 @@
 
 #include "core/propagationEngine.hpp"
 #include "invariantgraph/structure.hpp"
+#include "invariantgraphTest.hpp"
 
 class NodeTestBase : public testing::Test {
  protected:
@@ -72,17 +73,3 @@ inline void expectMarkedAsInput(
               1);
   }
 }
-
-#define FZN_VALUE(value) std::make_shared<fznparser::ValueLiteral>(value)
-#define FZN_SEARCH_VARIABLE(name, lowerBound, upperBound) \
-  std::make_shared<fznparser::SearchVariable>(            \
-      name, fznparser::AnnotationCollection(),            \
-      std::make_unique<fznparser::IntDomain>(lowerBound, upperBound))
-
-#define FZN_VECTOR_CONSTRAINT_ARG(...) \
-  std::vector<std::shared_ptr<fznparser::Literal>> { __VA_ARGS__ }
-
-#define FZN_NO_ANNOTATIONS fznparser::AnnotationCollection()
-#define FZN_DEFINES_VAR_ANNOTATION(name, variable) \
-  fznparser::MutableAnnotationCollection name;     \
-  (name).add<fznparser::DefinesVarAnnotation>(variable)
