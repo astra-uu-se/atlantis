190461ddf13881d87a81d31f51925193a1643674
Frej Knutar Lewander <60340613+frejknutarlewander@users.noreply.github.com>
2022-03-31 16:56:46 +0200

    Feature/all different except (#69)
    * AllDifferentExcept
    
    Added AllDifferentExcept constraint.
    
    * incCurrentTimestamp method
    
    added a method for increasing the current timestamp in the propgation engine. There was a bug where the modified search variables was not cleared properly.
    
    * removed comment
    
    removed comment I left in from a meeting
    
    * reverted commit
    
    * commented out test
    
    Commented out test that relies on fix on other branch

---
+++ include/constraints/allDifferentExcept.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+#include "constraint.hpp"
+#include "constraints/allDifferent.hpp"
+#include "core/types.hpp"
+#include "variables/intVar.hpp"
+
+class CommittableInt;  // forward declare
+class Engine;
+
+class AllDifferentExcept : public AllDifferent {
+ private:
+  std::vector<bool> _ignored;
+  Int _ignoredOffset;
+
+  bool isIgnored(Int) const;
+
+ public:
+  AllDifferentExcept(VarId violationId, std::vector<VarId> variables,
+                     std::vector<Int> ignored);
+
+  void recompute(Timestamp, Engine&) override;
+  void notifyInputChanged(Timestamp, Engine&, LocalId) override;
+};
+
+inline bool AllDifferentExcept::isIgnored(const Int val) const {
+  return _ignoredOffset <= val &&
+         static_cast<size_t>(val - _ignoredOffset) < _ignored.size() &&
+         _ignored[val - _ignoredOffset];
+}
