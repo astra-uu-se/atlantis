1ab1b32b967d28796456c2ef0705bf8edce74421
Maarten Flippo <maartenflippo@outlook.com>
2022-03-31 10:05:41 +0200

    Add test for the neighbourhood combinator.

---
+++ test/search/neighbourhoods/tNeighbourhoodCombinator.cpp
@@ -0,0 +1,73 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "search/neighbourhoods/neighbourhoodCombinator.hpp"
+
+class MockNeighbourhood : public search::neighbourhoods::Neighbourhood {
+ public:
+  MOCK_METHOD(void, initialise,
+              (search::RandomProvider&, search::AssignmentModifier&),
+              (override));
+
+  MOCK_METHOD(bool, randomMove,
+              (search::RandomProvider&, search::Assignment&, search::Annealer&),
+              (override));
+};
+
+class NeighbourhoodCombinatorTest : public ::testing::Test {
+ public:
+  std::vector<std::unique_ptr<search::neighbourhoods::Neighbourhood>> ns;
+  MockNeighbourhood* n1;
+  MockNeighbourhood* n2;
+
+  void SetUp() override {
+    auto unique_n1 = std::make_unique<MockNeighbourhood>();
+    auto unique_n2 = std::make_unique<MockNeighbourhood>();
+
+    n1 = unique_n1.get();
+    n2 = unique_n2.get();
+
+    ns.push_back(std::move(unique_n1));
+    ns.push_back(std::move(unique_n2));
+  }
+};
+
+TEST_F(NeighbourhoodCombinatorTest, initialise_calls_all_neighbourhoods) {
+  search::neighbourhoods::NeighbourhoodCombinator combinator(std::move(ns));
+
+  PropagationEngine engine;
+  search::RandomProvider random(123456789);
+
+  engine.beginMove();
+  search::AssignmentModifier modifier(engine);
+
+  EXPECT_CALL(*n1, initialise(::testing::Ref(random), ::testing::Ref(modifier)))
+      .Times(1);
+  EXPECT_CALL(*n2, initialise(::testing::Ref(random), ::testing::Ref(modifier)))
+      .Times(1);
+
+  combinator.initialise(random, modifier);
+  engine.endMove();
+}
+
+TEST_F(NeighbourhoodCombinatorTest,
+       randomMove_calls_one_neighbourhood_and_forwards_result) {
+  search::neighbourhoods::NeighbourhoodCombinator combinator(std::move(ns));
+
+  PropagationEngine engine;
+  search::RandomProvider random(123456789);
+  search::Assignment assignment(engine, NULL_ID, NULL_ID);
+  search::Annealer annealer(assignment, random);
+
+  EXPECT_CALL(*n1,
+              randomMove(::testing::Ref(random), ::testing::Ref(assignment),
+                         ::testing::Ref(annealer)))
+      .Times(0);
+
+  EXPECT_CALL(*n2,
+              randomMove(::testing::Ref(random), ::testing::Ref(assignment),
+                         ::testing::Ref(annealer)))
+      .WillOnce(::testing::Return(false));
+
+  combinator.randomMove(random, assignment, annealer);
+}
