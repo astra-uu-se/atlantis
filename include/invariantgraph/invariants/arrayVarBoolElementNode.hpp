3eaa93ce197be9891d352e2d28d50a8e76b02f76
Maarten Flippo <maartenflippo@outlook.com>
2022-04-13 10:07:53 +0200

    Implement array_var_bool_element natively.

---
+++ include/invariantgraph/invariants/arrayVarBoolElementNode.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <fznparser/model.hpp>
+
+#include "../structure.hpp"
+
+namespace invariantgraph {
+
+class ArrayVarBoolElementNode : public VariableDefiningNode {
+ private:
+  std::vector<VariableNode*> _as;
+  VariableNode* _b;
+
+ public:
+  static std::unique_ptr<ArrayVarBoolElementNode> fromModelConstraint(
+      const fznparser::FZNModel& model, const fznparser::Constraint& constraint,
+      const std::function<VariableNode*(MappableValue&)>& variableMap);
+
+  ArrayVarBoolElementNode(std::vector<VariableNode*> as, VariableNode* b,
+                          VariableNode* output)
+      : VariableDefiningNode({output}, {as}), _as(std::move(as)), _b(b) {
+    // No way to add this as an input in addition to the as vector. So we do it
+    // here explicitly.
+    b->markAsInputFor(static_cast<VariableDefiningNode*>(this));
+
+    b->imposeDomain(IntervalDomain{1, static_cast<Int>(_as.size())});
+    output->imposeDomain(SetDomain({0, 1}));
+  }
+
+  void registerWithEngine(
+      Engine& engine, VariableDefiningNode::VariableMap& variableMap) override;
+
+  [[nodiscard]] const std::vector<VariableNode*>& as() const noexcept {
+    return _as;
+  }
+  [[nodiscard]] VariableNode* b() const noexcept { return _b; }
+};
+
+}  // namespace invariantgraph
---
+++ test/invariantgraph/invariants/tArrayVarBoolElementNode.cpp
@@ -0,0 +1,74 @@
+#include "../nodeTestBase.hpp"
+#include "core/propagationEngine.hpp"
+#include "invariantgraph/invariants/arrayVarBoolElementNode.hpp"
+
+class ArrayVarBoolElementNodeTest : public NodeTestBase {
+ public:
+  BOOL_VARIABLE(a);
+  BOOL_VARIABLE(b);
+  BOOL_VARIABLE(c);
+
+  INT_VARIABLE(idx, 0, 10);
+  INT_VARIABLE(y, 0, 10);
+
+  fznparser::Constraint constraint{
+      "array_var_bool_element",
+      {idx.name, fznparser::Constraint::ArrayArgument{a.name, b.name, c.name},
+       y.name},
+      {}};
+
+  fznparser::FZNModel model{
+      {}, {a, b, c, idx, y}, {constraint}, fznparser::Satisfy{}};
+
+  std::unique_ptr<invariantgraph::ArrayVarBoolElementNode> node;
+
+  ArrayVarBoolElementNodeTest() : NodeTestBase(model) {}
+
+  void SetUp() override {
+    node = makeNode<invariantgraph::ArrayVarBoolElementNode>(constraint);
+  }
+};
+
+TEST_F(ArrayVarBoolElementNodeTest, construction) {
+  EXPECT_EQ(*node->b()->variable(),
+            invariantgraph::VariableNode::FZNVariable(idx));
+  EXPECT_EQ(node->definedVariables().size(), 1);
+  EXPECT_EQ(*node->definedVariables()[0]->variable(),
+            invariantgraph::VariableNode::FZNVariable(y));
+  expectMarkedAsInput(node.get(), {node->as()});
+  expectMarkedAsInput(node.get(), {node->b()});
+
+  EXPECT_EQ(node->as().size(), 3);
+  EXPECT_EQ(node->as()[0]->variable(),
+            invariantgraph::VariableNode::FZNVariable(a));
+  EXPECT_EQ(node->as()[1]->variable(),
+            invariantgraph::VariableNode::FZNVariable(b));
+  EXPECT_EQ(node->as()[2]->variable(),
+            invariantgraph::VariableNode::FZNVariable(c));
+}
+
+TEST_F(ArrayVarBoolElementNodeTest, application) {
+  PropagationEngine engine;
+  engine.open();
+  registerVariables(engine, {a.name, b.name, c.name, idx.name});
+  node->registerWithEngine(engine, _variableMap);
+  engine.close();
+
+  // The index ranges over the variable array (first index is 1).
+  EXPECT_EQ(engine.lowerBound(engineVariable(idx)), 1);
+  EXPECT_EQ(engine.upperBound(engineVariable(idx)), 3);
+
+  // The output domain should contain all values in all elements of the variable
+  // array.
+  EXPECT_EQ(engine.lowerBound(engineVariable(y)), 0);
+  EXPECT_EQ(engine.upperBound(engineVariable(y)), 1);
+
+  // a, b, c, idx
+  EXPECT_EQ(engine.searchVariables().size(), 4);
+
+  // a, b, c, idx, y
+  EXPECT_EQ(engine.numVariables(), 5);
+
+  // elementVar
+  EXPECT_EQ(engine.numInvariants(), 1);
+}
