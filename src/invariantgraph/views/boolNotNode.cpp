24ae015593afc2c90f987f900dc2a4c1e2fc62f4
Maarten Flippo <maartenflippo@outlook.com>
2022-04-13 11:26:31 +0200

    Implement bool_not natively.

---
+++ src/invariantgraph/views/boolNotNode.cpp
@@ -0,0 +1,20 @@
+#include "invariantgraph/views/boolNotNode.hpp"
+
+#include "../parseHelper.hpp"
+#include "views/bool2IntView.hpp"
+
+std::unique_ptr<invariantgraph::BoolNotNode>
+invariantgraph::BoolNotNode::fromModelConstraint(
+    const fznparser::FZNModel&, const fznparser::Constraint& constraint,
+    const std::function<VariableNode*(MappableValue&)>& variableMap) {
+  auto a = mappedVariable(constraint.arguments[0], variableMap);
+  auto b = mappedVariable(constraint.arguments[1], variableMap);
+
+  return std::make_unique<invariantgraph::BoolNotNode>(a, b);
+}
+
+void invariantgraph::BoolNotNode::registerWithEngine(
+    Engine& engine, VariableDefiningNode::VariableMap& variableMap) {
+  auto outputVar = engine.makeIntView<::Bool2IntView>(variableMap.at(_input));
+  variableMap.emplace(definedVariables()[0], outputVar);
+}
