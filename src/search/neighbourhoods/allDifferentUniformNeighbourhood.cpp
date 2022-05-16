2bb55fa788b347b89bdd319a20acda8215ff7654
Maarten Flippo <maartenflippo@outlook.com>
2022-05-16 17:12:31 +0200

    Start work on a circuit neighbourhood

--- src/search/neighbourhoods/allDifferentUniformNeighbourhood.cpp
+++ src/search/neighbourhoods/allDifferentUniformNeighbourhood.cpp
@@ -27,7 +27,7 @@ void search::neighbourhoods::AllDifferentUniformNeighbourhood::initialise(
   _freeVariables = _domain.size();
 
   for (auto const& variable : _variables) {
-    auto idx = random.intInRange(0, static_cast<Int>(_freeVariables) - 1);
+    auto idx = random.intInRange<size_t>(0, _freeVariables - 1);
     auto value = _domain[idx];
     modifications.set(variable.engineId(), value);
 
@@ -49,10 +49,9 @@ bool search::neighbourhoods::AllDifferentUniformNeighbourhood::randomMove(
 bool search::neighbourhoods::AllDifferentUniformNeighbourhood::swapValues(
     search::RandomProvider& random, search::Assignment& assignment,
     search::Annealer& annealer) {
-  size_t i = random.intInRange(0, static_cast<Int>(_variables.size()) - 1);
-  size_t j =
-      (i + random.intInRange(1, static_cast<Int>(_variables.size()) - 1)) %
-      _variables.size();
+  auto i = random.intInRange<size_t>(0, _variables.size() - 1);
+  auto j = (i + random.intInRange<size_t>(1, _variables.size() - 1)) %
+           _variables.size();
 
   auto var1 = _variables[i].engineId();
   auto var2 = _variables[j].engineId();
@@ -71,7 +70,7 @@ bool search::neighbourhoods::AllDifferentUniformNeighbourhood::assignValue(
   Int oldValue = assignment.value(var);
   size_t oldValueIdx = _domIndices[oldValue - _offset];
 
-  Int newValueIdx = random.intInRange(0, static_cast<Int>(_freeVariables) - 1);
+  auto newValueIdx = random.intInRange<size_t>(0, _freeVariables - 1);
   Int newValue = _domain[newValueIdx];
 
   if (maybeCommit(Move<1>({var}, {newValue}), assignment, annealer)) {
