b76f6c1e76280c5f95166ef3ec3548a4b24045f7
Maarten Flippo <maartenflippo@outlook.com>
2022-05-25 10:56:37 +0200

    Fix redefinition of alldifferent (#114)
    * Rename "alldifferent" to the appropriate "fzn_all_different_int".
    
    * Fix edge case for 1-sized arrays as inputs to all different.
    
    * Update tests.

--- src/invariantgraph/implicitConstraints/allDifferentImplicitNode.cpp
+++ src/invariantgraph/implicitConstraints/allDifferentImplicitNode.cpp
@@ -9,11 +9,18 @@ std::unique_ptr<invariantgraph::AllDifferentImplicitNode>
 invariantgraph::AllDifferentImplicitNode::fromModelConstraint(
     const fznparser::FZNModel& model, const fznparser::Constraint& constraint,
     const std::function<VariableNode*(MappableValue&)>& variableMap) {
-  assert(constraint.name == "alldifferent");
+  assert(constraint.name == "fzn_all_different_int");
   assert(constraint.arguments.size() == 1);
 
   auto variables =
       mappedVariableVector(model, constraint.arguments[0], variableMap);
+
+  if (variables.size() < 2) {
+    // Apparently it can happen that variables is an array of length 1. In that
+    // case, there is no benefit by the variable being defined by this implicit
+    // node, since any value from its domain would satisfy this constraint.
+    return nullptr;
+  }
 
   // For now, this only works when all the variables have the same domain.
   const auto& domain = variables.front()->domain();
