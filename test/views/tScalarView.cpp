b27bdf10a7eb4848ef1c821f18ea2f8a1263805d
Maarten Flippo <maartenflippo@outlook.com>
2022-05-25 11:13:30 +0200

    Add a view for x * c with x a variable and c a constant. (#112)

---
+++ test/views/tScalarView.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <rapidcheck/gtest.h>
+
+#include "core/propagationEngine.hpp"
+#include "views/scalarView.hpp"
+
+class ScalarViewTest : public ::testing::Test {
+ protected:
+  std::unique_ptr<PropagationEngine> engine;
+
+  void SetUp() override { engine = std::make_unique<PropagationEngine>(); }
+};
+
+RC_GTEST_FIXTURE_PROP(ScalarViewTest, simple, (Int a, Int b)) {
+  engine->open();
+  auto varId = engine->makeIntVar(a, a, a);
+  auto viewId = engine->makeIntView<ScalarView>(varId, b);
+  engine->close();
+
+  RC_ASSERT(engine->committedValue(viewId) == a * b);
+}
