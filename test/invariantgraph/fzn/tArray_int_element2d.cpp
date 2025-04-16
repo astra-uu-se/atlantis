9df1077c384a7f1475e4cd4ef47b4dae1b03a3dc
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2025-04-16 12:44:00 +0200

    array_int_element2d

--- test/invariantgraph/fzn/tArray_int_element2d.cpp
+++ test/invariantgraph/fzn/tArray_int_element2d.cpp
@@ -1,10 +1,12 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <string>
 #include <vector>
 
+#include "./fznTestBase.hpp"
 #include "atlantis/invariantgraph/fzn/array_int_element2d.hpp"
-#include "atlantis/invariantgraph/types.hpp"
+#include "atlantis/utils/domains.hpp"
 
 namespace atlantis::testing {
 
@@ -14,12 +16,189 @@ using ::testing::AtMost;
 using namespace atlantis::invariantgraph;
 using namespace atlantis::invariantgraph::fzn;
 
-class array_int_element2dTest : public ::testing::Test {
+class array_int_element2dTest : public FznTestBase {
  public:
   std::vector<VarNodeId> inputVarNodeIds{};
-  Int numInputs = 3;
+  std::string rowIndex{"rowIndex"};
+  std::string colIndex{"colIndex"};
+  std::vector<std::vector<Int>> parameters{};
+  std::string output{"output"};
+  Int rowOffset{1};
+  Int colOffset{1};
 
-  void SetUp() override {}
+  Int getValue(Int rowValue, Int colValue) const {
+    return parameters.at(rowValue - rowOffset).at(colValue - colOffset);
+  }
+
+  void generate() override {
+    const Int numRows = *rc::gen::inRange(1, 3);
+    const Int numCols = *rc::gen::inRange(1, 3);
+
+    constraintIdentifier = *rc::gen::arbitrary<bool>()
+                               ? "array_int_element2d"
+                               : "array_int_element2d_nonshifted_flat";
+
+    const Int rowLb = *rc::gen::element(-1024, -1, 0, 1, 1024);
+    addIntArg(rowLb, numRows + rowLb - 1, rowIndex);
+
+    const Int colLb = *rc::gen::element(-1024, -1, 0, 1, 1024);
+    addIntArg(colLb, numCols + colLb - 1, colIndex);
+
+    parameters = *rc::gen::container<std::vector<std::vector<Int>>>(
+        numRows, rc::gen::container<std::vector<Int>>(
+                     numCols, rc::gen::arbitrary<Int>()));
+
+    std::vector<Int> flatPars;
+    flatPars.reserve(numRows * numCols);
+    for (Int row = 0; row < numRows; row++) {
+      for (Int col = 0; col < numCols; col++) {
+        flatPars.emplace_back(parameters.at(row).at(col));
+      }
+    }
+
+    addArg(flatPars);
+
+    addIntArg(-1, -1, output);
+
+    addArg(numRows);
+    rowOffset = lowerBound(rowIndex);
+    addArg(rowOffset);
+    colOffset = lowerBound(colIndex);
+    addArg(colOffset);
+
+    generateConstraint();
+  }
+
+  [[nodiscard]] bool isSatisfied(bool committedValue) const override {
+    const Int rowIdxVal = intVal(rowIndex, committedValue);
+    const Int colIdxVal = intVal(colIndex, committedValue);
+    const Int expected =
+        parameters.at(rowIdxVal - rowOffset).at(colIdxVal - colOffset);
+    const Int actual = intVal(output, committedValue);
+    if (totalViolationVarId() != propagation::NULL_ID) {
+      return actual != expected;
+    }
+    return actual == expected;
+  }
+
+  [[nodiscard]] bool neverSatisfied() const override {
+    if (!isFixed(output)) {
+      const auto& outputNode = _invariantGraph->varNode(output);
+      if (!isFixed(rowIndex)) {
+        const auto& rowIdxNode = _invariantGraph->varNode(rowIndex);
+        return std::none_of(
+            rowIdxNode.constDomain()->begin(), rowIdxNode.constDomain()->end(),
+            [&](const Int rowVal) {
+              if (!isFixed(colIndex)) {
+                const auto& colIdxNode = _invariantGraph->varNode(colIndex);
+                return std::any_of(colIdxNode.constDomain()->begin(),
+                                    colIdxNode.constDomain()->end(),
+                                    [&](const Int colVal) {
+                                      return outputNode.inDomain(
+                                          getValue(rowVal, colVal));
+                                    });
+              }
+              return outputNode.inDomain(
+                  getValue(rowVal, intVal(colIndex)));
+            });
+      }
+      if (!isFixed(colIndex)) {
+        const auto& colIdxNode = _invariantGraph->varNode(colIndex);
+        return std::none_of(colIdxNode.constDomain()->begin(),
+                            colIdxNode.constDomain()->end(),
+                            [&](const Int colVal) {
+                              return outputNode.inDomain(
+                                  getValue(intVal(rowIndex), colVal));
+                            });
+
+      }
+      return !outputNode.inDomain(getValue(intVal(colIndex), intVal(rowIndex)));
+    }
+    if (!isFixed(rowIndex)) {
+      const auto& rowIdxNode = _invariantGraph->varNode(rowIndex);
+      if (!isFixed(colIndex)) {
+        const auto& colIdxNode = _invariantGraph->varNode(colIndex);
+        return std::none_of(
+            rowIdxNode.constDomain()->begin(), rowIdxNode.constDomain()->end(),
+            [&](const Int rowVal) {
+              return std::any_of(
+                  colIdxNode.constDomain()->begin(),
+                  colIdxNode.constDomain()->end(), [&](const Int colVal) {
+                    return intVal(output) == getValue(rowVal, colVal);
+                  });
+            });
+      }
+      return std::none_of(
+          rowIdxNode.constDomain()->begin(), rowIdxNode.constDomain()->end(),
+          [&](const Int rowVal) {
+            return getValue(rowVal, intVal(colIndex)) == intVal(output);
+          });
+    }
+    if (!isFixed(colIndex)) {
+      const auto& colIdxNode = _invariantGraph->varNode(colIndex);
+      return std::none_of(
+          colIdxNode.constDomain()->begin(), colIdxNode.constDomain()->end(),
+          [&](const Int colVal) {
+            return getValue(intVal(rowIndex), colVal) == intVal(output);
+          });
+    }
+    const Int expected = getValue(intVal(rowIndex), intVal(colIndex));
+    const Int actual = intVal(output);
+    return expected != actual;
+  }
+
+  [[nodiscard]] bool alwaysSatisfied() const override {
+    if (!isFixed(output)) {
+      return false;
+    }
+    if (!isFixed(rowIndex)) {
+      const auto& rowIdxNode = _invariantGraph->varNode(rowIndex);
+      if (!isFixed(colIndex)) {
+        const auto& colIdxNode = _invariantGraph->varNode(colIndex);
+        return std::all_of(
+            rowIdxNode.constDomain()->begin(), rowIdxNode.constDomain()->end(),
+            [&](const Int rowVal) {
+              return std::all_of(
+                  colIdxNode.constDomain()->begin(),
+                  colIdxNode.constDomain()->end(), [&](const Int colVal) {
+                    return intVal(output) == getValue(rowVal, colVal);
+                  });
+            });
+      }
+      return std::all_of(
+          rowIdxNode.constDomain()->begin(), rowIdxNode.constDomain()->end(),
+          [&](const Int rowVal) {
+            return getValue(rowVal, intVal(colIndex)) == intVal(output);
+          });
+    }
+    if (!isFixed(colIndex)) {
+      const auto& colIdxNode = _invariantGraph->varNode(colIndex);
+      return std::all_of(
+          colIdxNode.constDomain()->begin(), colIdxNode.constDomain()->end(),
+          [&](const Int colVal) {
+            return getValue(intVal(rowIndex), colVal) == intVal(output);
+          });
+    }
+    return getValue(intVal(rowIndex), intVal(colIndex)) == intVal(output);
+  }
+
+  [[nodiscard]] bool canMove() const override {
+    return !isFixed(rowIndex) || !isFixed(colIndex);
+  }
+
+  void move(bool committedValue) override {
+    if (!isFixed(rowIndex) && randBool()) {
+      changeValue(rowIndex, committedValue);
+    }
+    if (!isFixed(colIndex) && randBool()) {
+      changeValue(colIndex, committedValue);
+    }
+  }
+
+  void query() override { _solver->query(varId(output)); }
 };
 
+RC_GTEST_FIXTURE_PROP(array_int_element2dTest, RapidCheck, ()) {
+  rapidCheck();
+}
 }  // namespace atlantis::testing
