7bf285d674f69d3a9f0d7e6b5f661bf9bfb94a39
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2025-04-23 14:29:41 +0200

    bool clause

--- include/atlantis/invariantgraph/fzn/bool_clause.hpp
+++ include/atlantis/invariantgraph/fzn/bool_clause.hpp
@@ -13,11 +13,6 @@ bool bool_clause(FznInvariantGraph&,
                  const std::shared_ptr<fznparser::BoolVarArray>& as,
                  const std::shared_ptr<fznparser::BoolVarArray>& bs);
 
-bool bool_clause(FznInvariantGraph&,
-                 const std::shared_ptr<fznparser::BoolVarArray>& as,
-                 const std::shared_ptr<fznparser::BoolVarArray>& bs,
-                 const fznparser::BoolArg& reif);
-
 bool bool_clause(FznInvariantGraph&, const fznparser::Constraint&);
 
 }  // namespace atlantis::invariantgraph::fzn
--- test/invariantgraph/fzn/tBool_clause.cpp
+++ test/invariantgraph/fzn/tBool_clause.cpp
@@ -1,10 +1,12 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <rapidcheck/gtest.h>
 
+#include <algorithm>
 #include <vector>
 
+#include "./fznTestBase.hpp"
 #include "atlantis/invariantgraph/fzn/bool_clause.hpp"
-#include "atlantis/invariantgraph/types.hpp"
 
 namespace atlantis::testing {
 
@@ -14,12 +16,93 @@ using ::testing::AtMost;
 using namespace atlantis::invariantgraph;
 using namespace atlantis::invariantgraph::fzn;
 
-class bool_clauseTest : public ::testing::Test {
+class bool_clauseTest : public FznTestBase {
  public:
-  std::vector<VarNodeId> inputVarNodeIds{};
-  Int numInputs = 3;
+  std::vector<std::string> inputsA{};
+  std::vector<std::string> inputsB{};
 
-  void SetUp() override {}
+  [[nodiscard]] bool isSatisfied(bool committedValue) const override {
+    const bool expected =
+        std::ranges::any_of(inputsA,
+                            [&](const auto& input) {
+                              return boolVal(input, committedValue);
+                            }) ||
+        std::ranges::any_of(inputsB, [&](const auto& input) {
+          return !boolVal(input, committedValue);
+        });
+
+    RC_ASSERT(totalViolationVarId() != propagation::NULL_ID);
+    const bool shouldHold = violation(committedValue) == 0;
+    return shouldHold == expected;
+  }
+
+  [[nodiscard]] bool alwaysSatisfied() const override {
+    const bool alwaysSat =
+        std::ranges::any_of(inputsA,
+                            [&](const std::string& input) {
+                              return isFixedTo(input, bool{true});
+                            }) ||
+        std::ranges::any_of(inputsB, [&](const std::string& input) {
+          return isFixedTo(input, bool{false});
+        });
+    return alwaysSat;
+  }
+
+  [[nodiscard]] bool neverSatisfied() const override {
+    const bool alwaysUnsat =
+        std::ranges::all_of(inputsA,
+                            [&](const std::string& input) {
+                              return !inDomain(input, bool{true});
+                            }) &&
+        std::ranges::all_of(inputsB, [&](const std::string& input) {
+          return !inDomain(input, bool{false});
+        });
+    return alwaysUnsat;
+  }
+
+  void generate() override {
+    constraintIdentifier = "bool_clause";
+    const size_t sizeA = *rc::gen::inRange(0, 3);
+    const size_t sizeB = *rc::gen::inRange(0, 3);
+    inputsA.reserve(sizeA);
+    for (size_t i = 0; i < sizeA; i++) {
+      inputsA.emplace_back("b_a_" + std::to_string(i));
+    }
+    inputsB.reserve(sizeB);
+    for (size_t i = 0; i < sizeB; i++) {
+      inputsB.emplace_back("b_b_" + std::to_string(i));
+    }
+    addBoolVarArray(inputsA, "b_arr_a");
+    addBoolVarArray(inputsB, "b_arr_b");
+    generateConstraint();
+  }
+
+  [[nodiscard]] bool canMove() const override {
+    return std::ranges::any_of(inputsA,
+                               [&](const std::string& input) {
+                                 return varId(input) != propagation::NULL_ID;
+                               }) ||
+           std::ranges::any_of(inputsB, [&](const std::string& input) {
+             return varId(input) != propagation::NULL_ID;
+           });
+  }
+
+  void move(bool committedValue) override {
+    for (const auto& input : inputsA) {
+      if (varId(input) != propagation::NULL_ID && randBool()) {
+        changeValue(input, committedValue);
+      }
+    }
+    for (const auto& input : inputsB) {
+      if (varId(input) != propagation::NULL_ID && randBool()) {
+        changeValue(input, committedValue);
+      }
+    }
+  }
+
+  void query() override { _solver->query(totalViolationVarId()); }
 };
+
+RC_GTEST_FIXTURE_PROP(bool_clauseTest, RapidCheck, ()) { rapidCheck(); }
 
 }  // namespace atlantis::testing
