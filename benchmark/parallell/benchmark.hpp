5ab6ee6912076c8fb8e3c21d07130150204e4809
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2025-10-03 13:56:32 +0200

    added benchmarks

--- benchmark/parallell/benchmark.hpp
+++ benchmark/parallell/benchmark.hpp
@@ -2,81 +2,26 @@
 
 #include <functional>
 
-#include "atlantis/propagation/solver.hpp"
-#include "atlantis/propagation/types.hpp"
 #include "atlantis/types.hpp"
 
 namespace atlantis::benchmark {
 
-inline propagation::PropagationMode intToPropagationMode(Int state) {
+inline search::SearchType intToSearchType(Int state) {
   switch (state) {
-    case 3:
     case 2:
+      return search::SearchType::BEAMSEARCH;
     case 1:
-      return propagation::PropagationMode::OUTPUT_TO_INPUT;
+      return search::SearchType::PARALLEL;
     case 0:
     default:
-      return propagation::PropagationMode::INPUT_TO_OUTPUT;
+    return search::SearchType::BESTCOST;
   }
-}
-
-inline propagation::OutputToInputMarkingMode intToOutputToInputMarkingMode(
-    Int state) {
-  switch (state) {
-    case 3:
-      return propagation::OutputToInputMarkingMode::INPUT_TO_OUTPUT_EXPLORATION;
-    case 2:
-      return propagation::OutputToInputMarkingMode::OUTPUT_TO_INPUT_STATIC;
-    case 1:
-    case 0:
-    default:
-      return propagation::OutputToInputMarkingMode::NONE;
-  }
-}
-
-inline void setSolverMode(propagation::Solver& solver, const Int state) {
-  solver.setPropagationMode(intToPropagationMode(state));
-  solver.setOutputToInputMarkingMode(intToOutputToInputMarkingMode(state));
-}
-
-inline size_t rand_in_range(size_t minInclusive, size_t maxInclusive,
-                            std::mt19937& rng) {
-  return std::uniform_int_distribution<size_t>(minInclusive, maxInclusive)(rng);
-}
-
-inline bool all_in_range(size_t minInclusive, size_t maxExclusive,
-                         std::function<bool(size_t)>&& predicate) {
-  std::vector<size_t> vec(maxExclusive - minInclusive);
-  std::iota(vec.begin(), vec.end(), minInclusive);
-  return std::ranges::all_of(vec.begin(), vec.end(), std::move(predicate));
 }
 
 inline void defaultArguments(::benchmark::internal::Benchmark* benchmark) {
-  Int n = 16;
-  while (n <= 1024) {
-    for (Int mode = 0; mode <= 3; ++mode) {
-      benchmark->Args({n, mode});
-    }
-    if (n < 32) {
-      n += 16;
-    } else if (n < 128) {
-      n += 32;
-    } else if (n < 256) {
-      n += 64;
-    } else {
-      n *= 2;
-    }
-#ifndef NDEBUG
-    return;
-#endif
-  }
-}
-
-inline void defaultTreeArguments(::benchmark::internal::Benchmark* benchmark) {
-  Int treeHeight = 4;
-  for (Int argumentCount = 2; argumentCount <= 10; ++argumentCount) {
-    for (Int mode = 0; mode <= 3; ++mode) {
-      benchmark->Args({treeHeight, argumentCount, mode});
+  for (Int numThreads = 2; numThreads <= 16; numThreads *= 2) {
+    for (Int searchType = 0; searchType <= 2; ++searchType) {
+      benchmark->Args({numThreads, searchType});
     }
 #ifndef NDEBUG
     return;
