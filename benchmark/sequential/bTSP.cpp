3e6fa6186b87c2ff5ceb1519e0c3aeff17ced283
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2025-10-03 13:53:55 +0200

    moved propagation benchmarks to sequential folder

---
+++ benchmark/sequential/bTSP.cpp
@@ -0,0 +1,223 @@
+#include <benchmark/benchmark.h>
+
+#include <iostream>
+#include <random>
+#include <utility>
+#include <vector>
+
+#include "atlantis/propagation/invariants/elementVar.hpp"
+#include "atlantis/propagation/invariants/linear.hpp"
+#include "atlantis/propagation/invariants/plus.hpp"
+#include "atlantis/propagation/views/elementConst.hpp"
+#include "atlantis/propagation/views/intOffsetView.hpp"
+#include "atlantis/propagation/views/lessEqualConst.hpp"
+#include "atlantis/propagation/violationInvariants/lessEqual.hpp"
+#include "benchmark.hpp"
+
+namespace atlantis::benchmark {
+
+class TSP : public ::benchmark::Fixture {
+ public:
+  std::shared_ptr<propagation::Solver> solver;
+  std::vector<propagation::VarViewId> pred;
+  std::vector<propagation::VarViewId> timeToPred;
+  std::vector<std::vector<Int>> durations;
+  propagation::VarViewId totalDist{propagation::NULL_ID};
+
+  std::random_device rd;
+  std::mt19937 gen;
+
+  std::uniform_int_distribution<Int> distribution;
+  Int n{0};
+  const int MAX_TIME = 100000;
+
+  void SetUp(const ::benchmark::State& state) override {
+    solver = std::make_shared<propagation::Solver>();
+    n = state.range(0);
+
+    if (n < 3) {
+      throw std::runtime_error("There must be at least 3 locations.");
+    }
+
+    solver->open();
+
+    setSolverMode(*solver, static_cast<int>(state.range(1)));
+
+    for (Int i = 0; i < n; ++i) {
+      durations.emplace_back();
+      for (Int j = 0; j < n; ++j) {
+        durations.back().push_back(i * j);
+      }
+    }
+
+    pred.clear();
+    pred.reserve(n);
+
+    timeToPred.clear();
+    timeToPred.reserve(n);
+
+    for (Int i = 0; i < n; ++i) {
+      const Int initVal = (i - 1 + n) % n;
+      pred.emplace_back(
+          solver->makeIntVar(initVal, 0 + static_cast<Int>(i == 0),
+                             n - 1 - static_cast<Int>(i == n - 1)));
+      assert(solver->committedValue(pred.at(i)) == (i - 1 + n) % n);
+      assert(solver->currentValue(pred.at(i)) == (i - 1 + n) % n);
+    }
+
+    assert(isTourValid(false));
+    assert(isTourValid(true));
+
+    for (int i = 0; i < n; ++i) {
+      // timeToPred[i] = durations[i][pred[i]]
+      timeToPred.emplace_back(solver->makeIntView<propagation::ElementConst>(
+          *solver, pred[i], std::vector<Int>(durations[i]), 0));
+    }
+
+    // totalDist = sum(timeToPred)
+    totalDist = solver->makeIntVar(0, 0, MAX_TIME);
+    solver->makeInvariant<propagation::Linear>(
+        *solver, totalDist, std::vector<propagation::VarViewId>(timeToPred));
+
+    solver->close();
+    assert(std::ranges::all_of(pred.begin() + 1, pred.end(),
+                               [&](const propagation::VarViewId p) {
+                                 return solver->lowerBound(p) == 0;
+                               }));
+    assert(std::ranges::all_of(pred.begin(), pred.end() - 1,
+                               [&](const propagation::VarViewId p) {
+                                 return solver->upperBound(p) == n - 1;
+                               }));
+    assert(std::ranges::all_of(pred.begin(), pred.end(),
+                               [&](const propagation::VarViewId p) {
+                                 return 0 <= solver->committedValue(p) &&
+                                        solver->committedValue(p) < n;
+                               }));
+
+    gen = std::mt19937(rd());
+
+    distribution = std::uniform_int_distribution<Int>{0, n - 1};
+  }
+
+  void TearDown(const ::benchmark::State&) override {
+    pred.clear();
+    timeToPred.clear();
+  }
+
+  [[nodiscard]] bool isTourValid(bool committedValue) const {
+    std::vector<bool> visited(n, false);
+    Int cur = 0;
+    Int numVisited = 0;
+    while (!visited.at(cur)) {
+      ++numVisited;
+      visited.at(cur) = true;
+      cur = committedValue ? solver->committedValue(pred.at(cur))
+                           : solver->currentValue(pred.at(cur));
+      if (cur < 0 || n <= cur) {
+        return false;
+      }
+    }
+    return numVisited == n;
+  }
+
+  Int computeDistance() {
+    Int tot = 0;
+    for (Int i = 0; i < n; ++i) {
+      tot += durations.at(i).at(solver->currentValue(pred.at(i)));
+    }
+    return tot;
+  }
+};
+
+inline size_t randInRange(const size_t minVal, const size_t maxVal) {
+  return minVal + (std::rand() % (maxVal - minVal + 1));
+}
+
+BENCHMARK_DEFINE_F(TSP, probe_three_opt)(::benchmark::State& st) {
+  std::vector<Int> indexVec1(n);
+  std::iota(indexVec1.begin(), indexVec1.end(), 0);
+  std::vector<Int> indexVec2(n);
+  std::iota(indexVec2.begin(), indexVec2.end(), 0);
+
+  size_t probes = 0;
+  for ([[maybe_unused]] const auto& _ : st) {
+    Int i = static_cast<Int>(n);
+    Int j = static_cast<Int>(n);
+    for (size_t index1 = 0; i == n && index1 < static_cast<size_t>(n);
+         ++index1) {
+      std::swap(indexVec1[index1], indexVec1[randInRange(index1, n - 1)]);
+      const Int tmpI = indexVec1[index1];
+      for (size_t index2 = 0; index2 < static_cast<size_t>(n); ++index2) {
+        std::swap(indexVec2[index2],
+                  indexVec2[randInRange(index2, static_cast<size_t>(n - 1))]);
+        const Int tmpJ = indexVec2[index2];
+        if (tmpI != tmpJ && solver->committedValue(pred[tmpI]) != tmpJ) {
+          i = tmpI;
+          j = tmpJ;
+          break;
+        }
+      }
+    }
+    assert(i < static_cast<Int>(n));
+    assert(j < static_cast<Int>(n));
+    solver->beginMove();
+    solver->setValue(
+        pred[i], solver->committedValue(pred[solver->committedValue(pred[i])]));
+    solver->setValue(pred[j], solver->committedValue(pred[i]));
+    solver->setValue(pred[solver->committedValue(pred[i])],
+                     solver->committedValue(pred[j]));
+    solver->endMove();
+
+    solver->beginProbe();
+    solver->query(totalDist);
+    solver->endProbe();
+    ++probes;
+    assert(isTourValid(true));
+    assert(isTourValid(false));
+  }
+  st.counters["probes_per_second"] = ::benchmark::Counter(
+      static_cast<double>(probes), ::benchmark::Counter::kIsRate);
+}
+
+BENCHMARK_DEFINE_F(TSP, probe_all_relocate)(::benchmark::State& st) {
+  Int probes = 0;
+  for ([[maybe_unused]] const auto& _ : st) {
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (i == j || solver->committedValue(pred[i]) == j + 1) {
+          continue;
+        }
+        solver->beginMove();
+        solver->setValue(
+            pred[i],
+            solver->committedValue(pred[solver->committedValue(pred[i]) - 1]));
+        solver->setValue(pred[j], solver->committedValue(pred[i]));
+        solver->setValue(pred[solver->committedValue(pred[i]) - 1],
+                         solver->committedValue(pred[j]));
+        solver->endMove();
+
+        solver->beginProbe();
+        solver->query(totalDist);
+        solver->endProbe();
+        assert(solver->currentValue(totalDist) == computeDistance());
+        ++probes;
+        assert(isTourValid(true));
+        assert(isTourValid(false));
+      }
+    }
+  }
+  st.counters["probes_per_second"] = ::benchmark::Counter(
+      static_cast<double>(probes), ::benchmark::Counter::kIsRate);
+}
+
+//*
+BENCHMARK_REGISTER_F(TSP, probe_three_opt)
+    ->Unit(::benchmark::kMillisecond)
+    ->Apply(defaultArguments);
+
+/*
+BENCHMARK_REGISTER_F(TSP, probe_all_relocate)
+    ->Unit(::benchmark::kMillisecond)
+    ->Apply(defaultArguments);
+//*/
+}  // namespace atlantis::benchmark
---
+++ benchmark/sequential/benchmark.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <functional>
+
+#include "atlantis/propagation/solver.hpp"
+#include "atlantis/propagation/types.hpp"
+#include "atlantis/types.hpp"
+
+namespace atlantis::benchmark {
+
+inline propagation::PropagationMode intToPropagationMode(Int state) {
+  switch (state) {
+    case 3:
+    case 2:
+    case 1:
+      return propagation::PropagationMode::OUTPUT_TO_INPUT;
+    case 0:
+    default:
+      return propagation::PropagationMode::INPUT_TO_OUTPUT;
+  }
+}
+
+inline propagation::OutputToInputMarkingMode intToOutputToInputMarkingMode(
+    Int state) {
+  switch (state) {
+    case 3:
+      return propagation::OutputToInputMarkingMode::INPUT_TO_OUTPUT_EXPLORATION;
+    case 2:
+      return propagation::OutputToInputMarkingMode::OUTPUT_TO_INPUT_STATIC;
+    case 1:
+    case 0:
+    default:
+      return propagation::OutputToInputMarkingMode::NONE;
+  }
+}
+
+inline void setSolverMode(propagation::Solver& solver, const Int state) {
+  solver.setPropagationMode(intToPropagationMode(state));
+  solver.setOutputToInputMarkingMode(intToOutputToInputMarkingMode(state));
+}
+
+inline size_t rand_in_range(size_t minInclusive, size_t maxInclusive,
+                            std::mt19937& rng) {
+  return std::uniform_int_distribution<size_t>(minInclusive, maxInclusive)(rng);
+}
+
+inline bool all_in_range(size_t minInclusive, size_t maxExclusive,
+                         std::function<bool(size_t)>&& predicate) {
+  std::vector<size_t> vec(maxExclusive - minInclusive);
+  std::iota(vec.begin(), vec.end(), minInclusive);
+  return std::ranges::all_of(vec.begin(), vec.end(), std::move(predicate));
+}
+
+inline void defaultArguments(::benchmark::internal::Benchmark* benchmark) {
+  Int n = 16;
+  while (n <= 1024) {
+    for (Int mode = 0; mode <= 3; ++mode) {
+      benchmark->Args({n, mode});
+    }
+    if (n < 32) {
+      n += 16;
+    } else if (n < 128) {
+      n += 32;
+    } else if (n < 256) {
+      n += 64;
+    } else {
+      n *= 2;
+    }
+#ifndef NDEBUG
+    return;
+#endif
+  }
+}
+
+inline void defaultTreeArguments(::benchmark::internal::Benchmark* benchmark) {
+  Int treeHeight = 4;
+  for (Int argumentCount = 2; argumentCount <= 10; ++argumentCount) {
+    for (Int mode = 0; mode <= 3; ++mode) {
+      benchmark->Args({treeHeight, argumentCount, mode});
+    }
+#ifndef NDEBUG
+    return;
+#endif
+  }
+}
+
+}  // namespace atlantis::benchmark
