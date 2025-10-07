e55b595c4bca14b183da64975bb1a5862b960b91
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2025-10-07 11:10:38 +0200

    fixed issue in main

--- src/main.cpp
+++ src/main.cpp
@@ -100,8 +100,7 @@ int main(int argc, char* argv[]) {
     }
 
     if (result.count("annealing-schedule") == 1) {
-      backend.setAnnealingScheduleFactory(
-          atlantis::search::AnnealingScheduleFactory(
+      backend.setAnnealingScheduleFactory(std::make_shared<atlantis::search::AnnealingScheduleFactory>(
               result["annealing-schedule"].as<std::filesystem::path>()));
     }
 
