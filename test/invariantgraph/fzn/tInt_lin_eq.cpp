6c954db4c17741dc52047327d56edaff8bb61aff
Frej Knutar Lewander <60340613+frejknutarlewander@users.noreply.github.com>
2026-03-17 04:44:30 +0100

    fix to int_lin_eq test (#188)

--- test/invariantgraph/fzn/tInt_lin_eq.cpp
+++ test/invariantgraph/fzn/tInt_lin_eq.cpp
@@ -39,7 +39,7 @@ class int_lin_eqTest : public FznTestBase {
   [[nodiscard]] bool sameCoeff(Int& coeff) const {
     bool initialized = false;
     for (size_t i = 0; i < coeffs.size(); ++i) {
-      if (isFixed(inputs.at(i))) {
+      if (isFixed(inputs.at(i)) || coeffs.at(i) == 0) {
         continue;
       }
       if (initialized && coeff != std::abs(coeffs.at(i))) {
