9bf674f25c0ada567eead8739e34a36d753077bc
Frej Knutar Lewander <60340613+frejknutarlewander@users.noreply.github.com>
2026-04-07 13:23:41 +0200

    added TSPTW (#195)

---
+++ benchmark/parallell/bParTSPTW.cpp
@@ -0,0 +1,103 @@
+#include <benchmark/benchmark.h>
+
+#include <chrono>
+#include <filesystem>
+#include <iostream>
+#include <random>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "atlantis/fznBackend.hpp"
+#include "atlantis/invariantgraph/fznInvariantGraph.hpp"
+#include "atlantis/logging/logger.hpp"
+#include "benchmark.hpp"
+
+namespace atlantis::benchmark {
+
+class ParTSPTW : public ::benchmark::Fixture {
+ public:
+  static std::vector<std::string> instances;
+
+  std::shared_ptr<FznBackend> backend{nullptr};
+
+  long instance{-1};
+  long numThreads{0};
+  search::SearchType searchType{search::SearchType::BESTCOST};
+
+  std::vector<std::chrono::milliseconds> timelimits;
+
+  logging::Logger logger{stdout, logging::Level::LVL_ERROR};
+
+  static void populateInstances() {
+    instances = createInstances(std::string(FZN_DIR) + "/tsptw");
+  }
+
+  static size_t size() { return instances.size(); }
+
+  void SetUp(const ::benchmark::State& state) override {
+    instance = state.range(0);
+    numThreads = state.range(1);
+    searchType = intToSearchType(state.range(2));
+
+    timelimits = defaultTimelimits();
+
+    assert(0 <= instance && instance < static_cast<long>(instances.size()));
+
+    std::filesystem::path modelFilePath(instances.at(instance).c_str());
+    backend = std::make_shared<FznBackend>(logger, std::move(modelFilePath),
+                                           numThreads, searchType);
+  }
+
+  void TearDown(const ::benchmark::State&) override { backend = nullptr; }
+};
+
+std::vector<std::string> ParTSPTW::instances;
+
+BENCHMARK_DEFINE_F(ParTSPTW, run)(::benchmark::State& st) {
+  st.SetLabel(instances.at(instance));
+  std::vector<size_t> numSolutions(timelimits.size(), 0);
+  std::vector<size_t> bestObjective(timelimits.size(), 0);
+  std::vector<double> totalObjective(timelimits.size(), 0.0);
+  backend->setOnFinish([](FznBackend::SolveOutcome) {});
+  backend->setTimelimit(timelimits.back());
+
+  std::vector<std::chrono::time_point<std::chrono::steady_clock>> deadlines;
+  deadlines.reserve(timelimits.size());
+  for (const auto& tl : timelimits) {
+    deadlines.emplace_back(std::chrono::steady_clock::now() + tl);
+  }
+
+  backend->setOnSolution([&](const search::SavedAssignment& solution) {
+    for (size_t i = 0; i < timelimits.size(); i++) {
+      if (deadlines[i] < std::chrono::steady_clock::now()) {
+        continue;
+      }
+      ++numSolutions[i];
+      bestObjective[i] = solution.getCost().getObjective();
+      totalObjective[i] += static_cast<double>(bestObjective[i]);
+    }
+  });
+
+  for ([[maybe_unused]] const auto& _ : st) {
+    backend->solve(logger);
+    backend->join(logger);
+  }
+  for (size_t i = 0; i < timelimits.size(); i++) {
+    const std::string prefix = std::to_string(timelimits[i].count());
+    st.counters[prefix + "/solutions"] = static_cast<double>(numSolutions[i]);
+    st.counters[prefix + "/solutions_per_second"] = ::benchmark::Counter(
+        static_cast<double>(numSolutions[i]), ::benchmark::Counter::kIsRate);
+    st.counters[prefix + "/objective_best"] =
+        static_cast<double>(bestObjective[i]);
+    st.counters[prefix + "/objective_average"] =
+        totalObjective[i] / static_cast<double>(numSolutions[i]);
+  }
+}
+
+BENCHMARK_REGISTER_F(ParTSPTW, run)
+    ->Unit(::benchmark::kMillisecond)
+    ->Apply(defaultArguments<ParTSPTW>)
+    ->Iterations(1);
+
+}  // namespace atlantis::benchmark
---
+++ test/benchmarks/tFznTsptw.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+#include "modelTest.hpp"
+
+namespace atlantis::testing {
+
+std::unordered_set<Int> validObjectives() {
+  return std::unordered_set<Int>{153, 160, 162, 165, 171, 174, 177, 180, 187, 194, 195, 198, 199, 201};
+}
+
+TEST(FznTsptw, Solve) {
+  testModelFile("tsptw_6.fzn", validObjectives());
+}
+
+TEST(FznTsptw, Gecode) {
+  testModelFile("tsptw_6_gecode.fzn", validObjectives());
+}
+
+}  // namespace atlantis::testing
