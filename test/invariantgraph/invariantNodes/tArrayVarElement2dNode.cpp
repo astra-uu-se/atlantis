ee140986672ad42b19ecb41551d21d3f0fe367e2
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2026-05-13 10:44:22 +0200

    started with var_element2d

--- test/invariantgraph/invariantNodes/tArrayVarElement2dNode.cpp
+++ test/invariantgraph/invariantNodes/tArrayVarElement2dNode.cpp
@@ -8,14 +8,14 @@ using namespace atlantis::invariantgraph;
 class ArrayVarElement2dNodeTestFixture
     : public NodeTestBase<ArrayVarElement2dNode> {
  protected:
-  std::vector<std::vector<std::string>> varMatrix;
+  std::vector<std::vector<Var>> varMatrix;
 
-  std::string idx1Var{"idx1"};
-  std::string idx2Var{"idx2"};
-  std::string outputVar{"output"};
+  Var rowIdx{"rowIdx", std::vector<Int>{}, true};
+  Var colIdx{"colIdx", std::vector<Int>{}, true};
+  Var outputVar{"output", std::vector<Int>{}, true};
 
-  Int offsetIdx1 = -5;
-  Int offsetIdx2 = 5;
+  Int rowOffset = -5;
+  Int colOffset = 5;
 
   [[nodiscard]] bool isIntElement() const { return _paramData.data <= 2; }
 
@@ -29,38 +29,48 @@ class ArrayVarElement2dNodeTestFixture
 
   void SetUp() override {
     NodeTestBase::SetUp();
-    varMatrix = {{"x00", "x01"}, {"x10", "x11"}};
     if (isIntElement()) {
-      retrieveIntVarNode(-2, -1, varMatrix.at(0).at(0));
-      retrieveIntVarNode(-1, 0, varMatrix.at(0).at(1));
-      retrieveIntVarNode(0, 1, varMatrix.at(1).at(0));
-      retrieveIntVarNode(1, 2, varMatrix.at(1).at(1));
-      retrieveIntVarNode(-2, 2, outputVar);
+      varMatrix = std::vector<std::vector<Var>>{
+        {Var{"x00", -2, 1, true}, Var{"x01", -1, 0, true}},
+        {Var{"x10", 0, 1, true}, Var{"x11", 1, 2, true}}};
+      outputVar.isIntVar = true;
+      outputVar.domain = std::pair<Int, Int>(-2, 2);
+      retrieveIntVarNode(outputVar);
     } else {
-      for (const auto& row : varMatrix) {
-        for (const auto& identifier : row) {
-          retrieveBoolVarNode(identifier);
-        }
-      }
+      varMatrix = std::vector<std::vector<Var>>{
+          {Var{"x00", -2, 1, true}, Var{"x01", -1, 0, true}},
+          {Var{"x10", 0, 1, true}, Var{"x11", 1, 2, true}}};
+      outputVar.isIntVar = true;
+      outputVar.domain = std::pair<Int, Int>(0, 1);
       retrieveBoolVarNode(outputVar);
     }
 
-    retrieveIntVarNode(
-        offsetIdx1,
+    rowIdx.domain = std::pair<Int, Int>{
+        rowOffset,
         idx1ShouldBeReplaced()
-            ? offsetIdx1
-            : (offsetIdx1 + static_cast<Int>(varMatrix.size()) - 1),
-        idx1Var);
-    retrieveIntVarNode(
-        offsetIdx2,
+            ? rowOffset
+            : (rowOffset + static_cast<Int>(varMatrix.size()) - 1)};
+    colIdx.domain = std::pair<Int, Int>{
+        colOffset,
         idx2ShouldBeReplaced()
-            ? offsetIdx2
-            : (offsetIdx2 + static_cast<Int>(varMatrix.front().size()) - 1),
-        idx2Var);
+            ? colOffset
+            : (colOffset + static_cast<Int>(varMatrix.front().size()) - 1)};
 
-    createInvariantNode(*_invariantGraph, varNodeId(idx1Var),
-                        varNodeId(idx2Var), varNodeIds(varMatrix),
-                        varNodeId(outputVar), offsetIdx1, offsetIdx2);
+    for (const auto& row : varMatrix) {
+      for (const auto& v : row) {
+        if (isIntElement()) {
+          retrieveIntVarNode(v);
+        } else {
+          retrieveBoolVarNode(v);
+        }
+      }
+    }
+    retrieveIntVarNode(rowIdx);
+    retrieveIntVarNode(colIdx);
+
+    createInvariantNode(*_invariantGraph, varNodeId(rowIdx),
+                        varNodeId(colIdx), varNodeIds(varMatrix),
+                        varNodeId(outputVar), rowOffset, colOffset);
   }
 };
 
@@ -91,7 +101,7 @@ TEST_P(ArrayVarElement2dNodeTestFixture, propagation) {
   std::vector<propagation::VarViewId> inputVarIds;
   std::vector<Int> inputVals;
 
-  for (const auto& idx : std::array<std::string, 2>{idx1Var, idx2Var}) {
+  for (const auto& idx : std::array<std::string, 2>{rowIdx.identifier, colIdx.identifier}) {
     inputVarIds.emplace_back(varNode(idx).isFixed() ? propagation::NULL_ID
                                                     : varId(idx));
     inputVals.emplace_back(inputVarIds.back() == propagation::NULL_ID
@@ -120,8 +130,8 @@ TEST_P(ArrayVarElement2dNodeTestFixture, propagation) {
     _solver->endProbe();
 
     const Int actual = _solver->currentValue(outputId);
-    const Int row = inputVals.at(0) - offsetIdx1;
-    const Int col = inputVals.at(1) - offsetIdx2;
+    const Int row = inputVals.at(0) - rowOffset;
+    const Int col = inputVals.at(1) - colOffset;
 
     const Int index =
         2 + (row * static_cast<Int>(varMatrix.front().size()) + col);
