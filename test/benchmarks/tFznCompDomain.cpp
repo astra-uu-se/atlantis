a62671351bc3ec798d22e8d58151b541269d1ef4
Frej Knutar Lewander <frej.knutar.lewander@it.uu.se>
2026-08-05 15:04:51 +0200

    refactoring mzn,dzn,fzn
    fixing the folder structure of mzn, dzn, and fzn files
    improving compilation of fzn files for testing and benchmarking

---
+++ test/benchmarks/tFznCompDomain.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+#include "modelTest.hpp"
+
+namespace atlantis::testing {
+
+TEST(FznCarSequencing, DISABLED_Solve) { testModelFile("car_sequencing.fzn"); }
+
+}  // namespace atlantis::testing
